c2e6c44644e95c0fbf5be49c1b58c1fa01e5c38e
Asbert Ma <stayinalive@msn.cn>
2018-08-20 18:27:58 +0800

    add webview component

---
+++ src/renderer/components/ViewPort.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <webview :preload="preload" :src="url"></webview>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ViewPort extends Vue {
+  preload: string = 'file:///' + (window as any).__preload
+  @Prop() private url?: string
+}
+</script>
+
