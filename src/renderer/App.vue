9a20118d3ff31251e8c6fc296f68fd7513c4c0bb
Asbert Ma <stayinalive@msn.cn>
2018-08-24 16:43:18 +0800

    add browser like things, tab and webview

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,29 +1,103 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Tabbar @switch="switchTab" class="sync-drag-zone" :tabs="ports" :currentTab="currentPortId" @close="removeTab"
+    />
+    <div class="sync-container">
+      <viewport class="viewport-layout" v-for="item in ports" :key="item.id" :currentPort="currentPortId" :instanceId="item.portId" :url="item.url"
+        @title-updated="updateTitle" @favicon-updated="updateFavicon" @new-tab="newTab" />
     </div>
-    <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Tabbar from './components/tabBar.vue'
+import { Component, Vue } from 'vue-property-decorator'
+import viewport, { portData } from './components/ViewPort.vue'
+@Component({
+  components: {
+    Tabbar,
+    viewport
+  }
+})
+export default class App extends Vue {
+  private ports: portData[] = [{ portId: Date.now() }]
+  private currentPortId?: number = this.ports[0]['portId']
+
+  public addPort(data: portData) {
+    let now = Date.now()
+    this.currentPortId = now
+    this.ports.push({ portId: data.portId || now, url: data.url })
+  }
+  public updateTitle(data: portData) {
+    let index = this.ports.findIndex(item => {
+      return item.portId === data.portId
+    })
+
+    this.$set(this.ports[index], 'contentId', data.contentId)
+    this.$set(this.ports[index], 'title', data.title)
+  }
+  public switchTab(portId: number) {
+    this.currentPortId = portId
+  }
+  public newTab(data: portData) {
+    this.addPort({
+      url: data.url
+    })
+  }
+
+  public removeTab(portId: number) {
+    let index = this.ports.findIndex(item => {
+      return item.portId === portId
+    })
+
+    if (index >= 0) {
+      this.ports.splice(index, 1)
+    }
+    if (this.ports.length === 0) {
+      this.ports.push({
+        portId: Date.now(),
+        title: 'New tab'
+      })
+    } else {
+      this.currentPortId = this.ports[index - 1]['portId']
+    }
+  }
+
+  public updateFavicon(data: portData) {
+    let index = this.ports.findIndex(item => {
+      return item.portId === data.portId
+    })
+    this.$set(this.ports[index], 'contentId', data.contentId)
+    this.$set(this.ports[index], 'icon', data.icons[0])
+  }
+}
+</script>
 
 <style lang="scss">
+body {
+  background: #fff;
+  height: 100vh;
+  width: 100vw;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  width: 100%;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.sync-drag-zone {
+  -webkit-app-region: drag;
+}
+.sync-tab {
+  width: 200px;
+  background-color: #00796b;
+}
+.sync-container {
+  height: calc(100% - 41px);
+  position: relative;
+}
+.sync-container .viewport-layout {
+  position: absolute;
 }
 </style>
---
+++ src/renderer/components/tabBar.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="sync-tab-bar">
+    <div @click.stop="switchTab(item.portId)" class="sync-tab" :class="{'current-tab': item.portId === currentTab}" v-for="item in tabs" :key="item.id">
+      <div class="tab-container">
+        <img :src="item.icon">
+        <span>{{item.title}}</span>
+        <v-icon @click.stop="popClose(item.portId)" class="tab-close">close</v-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component
+export default class Tabbar extends Vue {
+  @Prop() private tabs!: any[]
+  @Prop() private currentTab!: number
+  public popClose(portId: number) {
+    this.$emit('close', portId)
+  }
+
+  public switchTab(portId: number) {
+    this.$emit('switch', portId)
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.sync-tab-bar {
+  padding-top: 10px;
+  padding-right: 5px;
+  height: 41px;
+  width: 100%;
+  -webkit-user-select: none;
+  background-color: #cfd8dc;
+  transition: padding-left 300ms ease-in-out;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  flex-direction: row;
+}
+.darwin .sync-tab-bar {
+  padding-left: 80px;
+}
+.full-screen .sync-tab-bar {
+  padding-left: 5px;
+}
+.sync-tab {
+  padding-top: 5px;
+  width: 150x;
+  height: 30px;
+  -webkit-user-select: none;
+  -webkit-app-region: no-drag;
+  background-color: rgb(220, 226, 228);
+  border-top-left-radius: 7px;
+  border-top-right-radius: 7px;
+  // border-color: transparent;
+  border: 1px solid #eee;
+  cursor: normal;
+}
+.sync-tab.current-tab {
+  background-color: #fff;
+}
+.sync-tab img {
+  width: 15px;
+  height: 15px;
+  margin-top: 2px;
+}
+.sync-tab span {
+  font-size: 13px;
+  max-width: 140px;
+  height: 20px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.sync-tab .tab-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.tab-close {
+  color: rgba(0, 0, 0, 0.29);
+  font-size: 18px;
+}
+</style>
