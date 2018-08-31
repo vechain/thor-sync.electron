8fc09275d5fbfca638b857a0470f881a7bf8ff88
Asbert Ma <stayinalive@msn.cn>
2018-08-31 18:01:01 +0800

    buildin dapp list

---
+++ src/renderer/components/AppList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sync-dapp-list">
+    <slot/>
+    <div v-for="(item, i) in list" :key="i">
+
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Prop } from 'vue-property-decorator'
+export interface DApp {
+  icon: string,
+  url: string,
+  name: string
+}
+export class AppList extends Vue {
+  @Prop({default: []})
+  private list!: DApp[]
+}
+</script>
+
