07ed7d7298c8b62e207689ad435e1738a5a9a66c
Asbert Ma <stayinalive@msn.cn>
2018-09-04 14:48:19 +0800

    add tab style

--- src/base/browser/eventTransfer.ts
+++ src/base/browser/eventTransfer.ts
@@ -2,17 +2,16 @@ import { ipcRenderer } from 'electron'
 import { syncEventTarget, SyncEvent } from '@/base/browser/eventsDefine'
 import { EventNames, EventData } from '@/base/eventConst'
 
-let Win = window as any
+const Win = window as any
 
-ipcRenderer.on(EventNames.sync_channel, function(e: any, data: EventData) {
-  console.log(data)
+ipcRenderer.on(EventNames.sync_channel, (e: any, data: EventData) => {
   if (
     process.env.NODE_ENV === 'development' ||
     Win.DAppStatus === 'debug'
   ) {
-    console.log('Event Name:' + data.name, 'Event Data:' + data.data)
+    // console.log('Event Name:' + data.name, 'Event Data:' + data.data)
   }
-  let event = new SyncEvent(data.name, data.data)
+  const event = new SyncEvent(data.name, data.data)
   syncEventTarget.dispatchEvent(event)
 })
 
