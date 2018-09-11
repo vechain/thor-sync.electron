d86322f83077cde573aa0882cfa6426c3ff18ffe
qianbin <cola.tin.com@gmail.com>
2018-09-11 13:41:31 +0800

    rename Endpoint to Connection

--- src/preload.ts
+++ src/preload.ts
@@ -1,9 +1,9 @@
 import { remote } from 'electron'
 const webContentsId = remote.getCurrentWebContents().id
-const ep = remote.app.backend.connect(webContentsId)
+const conn = remote.app.backend.connect(webContentsId)
 
 Object.defineProperty(window, 'connex', {
     enumerable: true,
-    value: ep.createConnex()
+    value: conn.createConnex()
 })
 
