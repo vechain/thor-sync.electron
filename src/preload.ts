3ab1fb214b29486e736c3d055b2f790a9d3018e9
Asbert Ma <stayinalive@msn.cn>
2018-09-11 14:10:07 +0800

    open dev tool in dev env

--- src/preload.ts
+++ src/preload.ts
@@ -2,8 +2,11 @@ import { remote } from 'electron'
 const webContentsId = remote.getCurrentWebContents().id
 const conn = remote.app.backend.connect(webContentsId)
 
+if (process.env.NODE_ENV === 'development') {
+  remote.getCurrentWebContents().openDevTools()
+}
+
 Object.defineProperty(window, 'connex', {
     enumerable: true,
     value: conn.createConnex()
 })
-
