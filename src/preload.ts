898a8ffe00134da0848eed8fa2608f0ae8b1fbb0
Asbert Ma <stayinalive@msn.cn>
2018-09-11 14:10:07 +0800

    open dev tool in dev env

--- src/preload.ts
+++ src/preload.ts
@@ -22,6 +22,10 @@ const partition = webContents.getWebPreferences().partition
 
 const userAddr = partition ? new URL(partition).pathname.split('/').pop() : undefined
 
+if (process.env.NODE_ENV === 'development') {
+  remote.getCurrentWebContents().openDevTools()
+}
+
 Object.defineProperty(window, 'connex', {
     enumerable: true,
     value: remote.app.backend.connect(webContents.id, userAddr)
