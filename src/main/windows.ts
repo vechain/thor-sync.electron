67d9845fcc820b920c67b149d4524c0aaf4c633f
qianbin <cola.tin.com@gmail.com>
2018-09-13 01:42:35 +0800

    add attach createNovaWindow to app object rewrite preload file

--- src/main/windows.ts
+++ src/main/windows.ts
@@ -1,9 +1,10 @@
-import { BrowserWindow } from 'electron'
+import { BrowserWindow, app } from 'electron'
+import { siteConfigs } from './backend'
 
 const mainUrl =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:9080`
-    : `file://${__dirname}/index.html`
+    process.env.NODE_ENV === 'development'
+        ? `http://localhost:9080`
+        : `file://${__dirname}/index.html`
 
 // const proxyUrl =
 //   process.env.NODE_ENV === 'development'
@@ -14,25 +15,25 @@ let mainWin: Electron.BrowserWindow | null
 // let proxyWin: Electron.BrowserWindow | null
 
 function createMainWin(): Electron.BrowserWindow {
-  let opts: Electron.BrowserWindowConstructorOptions = {
-    height: 600,
-    useContentSize: true,
-    show: false,
-    width: 800,
-    minWidth: 800,
-    minHeight: 600
-  }
+    let opts: Electron.BrowserWindowConstructorOptions = {
+        height: 600,
+        useContentSize: true,
+        show: false,
+        width: 800,
+        minWidth: 800,
+        minHeight: 600
+    }
 
-  switch (process.platform) {
-    case 'darwin':
-      opts.titleBarStyle = 'hiddenInset'
-      break
-    default:
-      // TODO No logic for now
-      break
-  }
+    switch (process.platform) {
+        case 'darwin':
+            opts.titleBarStyle = 'hiddenInset'
+            break
+        default:
+            // TODO No logic for now
+            break
+    }
 
-  return new BrowserWindow(opts)
+    return app.createNovaWindow(siteConfigs[0], opts)
 }
 
 // function createProxyWin(): Electron.BrowserWindow {
@@ -42,28 +43,28 @@ function createMainWin(): Electron.BrowserWindow {
 //   })
 // }
 
-export const createWin = function() {
-  // proxyWin = createProxyWin()
-  mainWin = createMainWin()
+export const createWin = function () {
+    // proxyWin = createProxyWin()
+    mainWin = createMainWin()
 
-  // proxyWin.loadURL(proxyUrl)
+        // proxyWin.loadURL(proxyUrl)
 
-  // proxyWin.once('ready-to-show', () => {
-  //   ;(mainWin as Electron.BrowserWindow).loadURL(mainUrl)
-  // })
-  ;(mainWin as Electron.BrowserWindow).loadURL(mainUrl)
-  // proxyWin.on('closed', () => {
-  //   proxyWin = null
-  // })
+        // proxyWin.once('ready-to-show', () => {
+        //   ;(mainWin as Electron.BrowserWindow).loadURL(mainUrl)
+        // })
+        ; (mainWin as Electron.BrowserWindow).loadURL(mainUrl)
+    // proxyWin.on('closed', () => {
+    //   proxyWin = null
+    // })
 
-  mainWin.on('closed', () => {
-    // if (proxyWin) {
-    //   proxyWin.close()
-    // }
-    mainWin = null
-  })
+    mainWin.on('closed', () => {
+        // if (proxyWin) {
+        //   proxyWin.close()
+        // }
+        mainWin = null
+    })
 
-  mainWin.once('ready-to-show', () => {
-    ;(mainWin as Electron.BrowserWindow).show()
-  })
+    mainWin.once('ready-to-show', () => {
+        ; (mainWin as Electron.BrowserWindow).show()
+    })
 }
