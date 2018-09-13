164a14d05f1a66ff2ed54cea66360f1c15639115
Asbert Ma <stayinalive@msn.cn>
2018-09-13 18:32:28 +0800

    coding style

--- src/dapps/insight/App.vue
+++ src/dapps/insight/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
-    Insight
-    <div><pre>{{block}}</pre></div>
-  </div>
-  
+    <div>
+        Insight
+        <div><pre>{{block}}</pre></div>
+    </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 @Component
-export default class App extends Vue{
-  name: string = 'app'
-  block = JSON.stringify(connex.thor.bestBlock,null, 4)
-  
-  created() {
-    (async () => {
-    for ( ;;) {
-      await  connex.thor.nextTick()
-      this.block = JSON.stringify(connex.thor.bestBlock,null, 4)
+export default class App extends Vue {
+    name: string = 'app'
+    block = JSON.stringify(connex.thor.bestBlock, null, 4)
+
+    created() {
+        ;(async () => {
+            for (;;) {
+                await connex.thor.nextTick()
+                this.block = JSON.stringify(connex.thor.bestBlock, null, 4)
+            }
+        })()
     }
-    })()
-  }  
 }
 </script>
 
--- src/dapps/wallet/App.vue
+++ src/dapps/wallet/App.vue
@@ -7,8 +7,8 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 @Component
-export default class App extends Vue{
-  name: string = 'app'
+export default class App extends Vue {
+    name: string = 'app'
 }
 </script>
 
