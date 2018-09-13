bc6a1db425f2ee1d7c968e9f7f06f100dc7058fd
qianbin <cola.tin.com@gmail.com>
2018-09-13 13:48:07 +0800

    fix build script

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -11,6 +11,7 @@ const Multispinner = require('multispinner')
 const mainConfig = require('./webpack.main.config')
 const rendererConfig = require('./webpack.renderer.config')
 const dappsConfig = require('./webpack.dapps.config')
+const preloadConfig = require('./webpack.preload.config')
 // const webConfig = require('./webpack.web.config')
 
 const doneLog = chalk.bgGreen.white(' DONE ') + ' '
@@ -22,88 +23,97 @@ if (process.env.BUILD_TARGET === 'clean') clean()
 // else if (process.env.BUILD_TARGET === 'web') web()
 else build()
 
-function clean () {
-  del.sync(['build/*', '!build/icons', '!build/icons/icon.*'])
-  console.log(`\n${doneLog}\n`)
-  process.exit()
-}
-
-function build () {
-  greeting()
-
-  del.sync(['dist/electron/*', '!.gitkeep'])
-
-  const tasks = ['main', 'DApps', 'renderer']
-  const m = new Multispinner(tasks, {
-    preText: 'building',
-    postText: 'process'
-  })
-
-  let results = ''
-
-  m.on('success', () => {
-    process.stdout.write('\x1B[2J\x1B[0f')
-    console.log(`\n\n${results}`)
-    console.log(`${okayLog}take it away ${chalk.yellow('`electron-builder`')}\n`)
+function clean() {
+    del.sync(['build/*', '!build/icons', '!build/icons/icon.*'])
+    console.log(`\n${doneLog}\n`)
     process.exit()
-  })
-
-  pack(mainConfig).then(result => {
-    results += result + '\n\n'
-    m.success('main')
-  }).catch(err => {
-    m.error('main')
-    console.log(`\n  ${errorLog}failed to build main process`)
-    console.error(`\n${err}\n`)
-    process.exit(1)
-  })
-
-  pack(rendererConfig).then(result => {
-    results += result + '\n\n'
-    m.success('renderer')
-  }).catch(err => {
-    m.error('renderer')
-    console.log(`\n  ${errorLog}failed to build renderer process`)
-    console.error(`\n${err}\n`)
-    process.exit(1)
-  })
-
-  pack(dappsConfig).then(result => {
-    results += result + '\n\n'
-    m.success('DApps')
-  }).catch(err => {
-    m.error('DApps')
-    console.log(`\n  ${errorLog}failed to build renderer process`)
-    console.error(`\n${err}\n`)
-    process.exit(1)
-  })
 }
 
-function pack (config) {
-  return new Promise((resolve, reject) => {
-    webpack(config, (err, stats) => {
-      if (err) reject(err.stack || err)
-      else if (stats.hasErrors()) {
-        let err = ''
+function build() {
+    greeting()
 
-        stats.toString({
-          chunks: false,
-          colors: true
-        })
-        .split(/\r?\n/)
-        .forEach(line => {
-          err += `    ${line}\n`
-        })
+    del.sync(['dist/electron/*', '!.gitkeep'])
 
-        reject(err)
-      } else {
-        resolve(stats.toString({
-          chunks: false,
-          colors: true
-        }))
-      }
+    const tasks = ['main', 'DApps', 'renderer', 'preload']
+    const m = new Multispinner(tasks, {
+        preText: 'building',
+        postText: 'process'
     })
-  })
+
+    let results = ''
+
+    m.on('success', () => {
+        process.stdout.write('\x1B[2J\x1B[0f')
+        console.log(`\n\n${results}`)
+        console.log(`${okayLog}take it away ${chalk.yellow('`electron-builder`')}\n`)
+        process.exit()
+    })
+
+    pack(mainConfig).then(result => {
+        results += result + '\n\n'
+        m.success('main')
+    }).catch(err => {
+        m.error('main')
+        console.log(`\n  ${errorLog}failed to build main process`)
+        console.error(`\n${err}\n`)
+        process.exit(1)
+    })
+
+    pack(rendererConfig).then(result => {
+        results += result + '\n\n'
+        m.success('renderer')
+    }).catch(err => {
+        m.error('renderer')
+        console.log(`\n  ${errorLog}failed to build renderer process`)
+        console.error(`\n${err}\n`)
+        process.exit(1)
+    })
+
+    pack(dappsConfig).then(result => {
+        results += result + '\n\n'
+        m.success('DApps')
+    }).catch(err => {
+        m.error('DApps')
+        console.log(`\n  ${errorLog}failed to build dapp process`)
+        console.error(`\n${err}\n`)
+        process.exit(1)
+    })
+    pack(preloadConfig).then(result => {
+        results += result + '\n\n'
+        m.success('preload')
+    }).catch(err => {
+        m.error('preload')
+        console.log(`\n  ${errorLog}failed to build preload process`)
+        console.error(`\n${err}\n`)
+        process.exit(1)
+    })
+}
+
+function pack(config) {
+    return new Promise((resolve, reject) => {
+        webpack(config, (err, stats) => {
+            if (err) reject(err.stack || err)
+            else if (stats.hasErrors()) {
+                let err = ''
+
+                stats.toString({
+                    chunks: false,
+                    colors: true
+                })
+                    .split(/\r?\n/)
+                    .forEach(line => {
+                        err += `    ${line}\n`
+                    })
+
+                reject(err)
+            } else {
+                resolve(stats.toString({
+                    chunks: false,
+                    colors: true
+                }))
+            }
+        })
+    })
 }
 
 // function web () {
@@ -120,20 +130,20 @@ function pack (config) {
 //   })
 // }
 
-function greeting () {
-  const cols = process.stdout.columns
-  let text = ''
+function greeting() {
+    const cols = process.stdout.columns
+    let text = ''
 
-  if (cols > 85) text = 'lets-build'
-  else if (cols > 60) text = 'lets-|build'
-  else text = false
+    if (cols > 85) text = 'lets-build'
+    else if (cols > 60) text = 'lets-|build'
+    else text = false
 
-  if (text && !isCI) {
-    say(text, {
-      colors: ['yellow'],
-      font: 'simple3d',
-      space: false
-    })
-  } else console.log(chalk.yellow.bold('\n  lets-build'))
-  console.log()
+    if (text && !isCI) {
+        say(text, {
+            colors: ['yellow'],
+            font: 'simple3d',
+            space: false
+        })
+    } else console.log(chalk.yellow.bold('\n  lets-build'))
+    console.log()
 }
