853fe948e7871bd5333237bcd49edc73304e3557
qianbin <cola.tin.com@gmail.com>
2018-09-13 19:28:04 +0800

    add menu setup stuff

---
+++ src/main/menu.ts
@@ -0,0 +1,113 @@
+import { app, Menu, MenuItemConstructorOptions } from 'electron'
+
+// this template is copied from https://electronjs.org/docs/api/menu
+const template: MenuItemConstructorOptions[] = [
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'close' }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click() { require('electron').shell.openExternal('https://electronjs.org') }
+            }
+        ]
+    }
+]
+
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Window',
+                accelerator: 'Cmd+N',
+                click() {
+                    app.createWindow()
+                }
+            }]
+    })
+
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services', submenu: [] },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+        ]
+    });
+
+    // Edit menu
+    (template[2].submenu as MenuItemConstructorOptions[]).push(
+        { type: 'separator' },
+        {
+            label: 'Speech',
+            submenu: [
+                { role: 'startspeaking' },
+                { role: 'stopspeaking' }
+            ]
+        }
+    )
+
+    // Window menu
+    template[3].submenu = [
+        { role: 'close' },
+        { role: 'minimize' },
+        { role: 'zoom' },
+        { type: 'separator' },
+        { role: 'front' }
+    ]
+}
+
+const dockTemplate: MenuItemConstructorOptions[] = [{
+    label: 'New Window',
+    click() {
+        app.createWindow()
+    }
+}]
+
+export function setupMenu() {
+    if (process.platform === 'darwin') {
+        app.dock.setMenu(Menu.buildFromTemplate(dockTemplate))
+    }
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+}
