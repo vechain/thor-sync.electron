6fb5e21c53123b6c7630d3ad4bea7ccb1c1961b3
qianbin <cola.tin.com@gmail.com>
2018-09-16 00:33:54 +0800

    split Connex.Thor.Site declaration

---
+++ src/connex/connex.thor.site.d.ts
@@ -0,0 +1,26 @@
+declare namespace Connex{
+    namespace Thor {
+        interface Site {
+            readonly url: string
+            readonly genesisBlock: Connex.Thor.Block
+            readonly syncProgress: number
+            nextTick(): Promise<void>
+
+            withWireAgent(agent?: any): Site
+            createWire(): Site.Wire
+        }
+        namespace Site {
+            interface Wire {
+                readonly url: string
+                get<T>(path: string, query?: object): Promise<T>
+                post<T>(path: string, data: object, query?: object): Promise<T>
+                ws(path: string, query?: object): WebSocket
+            }
+
+            interface WebSocket {
+                close(): void
+                read(): Promise<string | Buffer | ArrayBuffer | Buffer[]>
+            }
+        }
+    }
+}
