706002f983aea9c96873b9df48acbb2e8afff01f
qianbin <cola.tin.com@gmail.com>
2018-09-15 17:27:22 +0800

    make Deferred impl PromiseLike & add reject method

--- src/base/deferred.ts
+++ src/base/deferred.ts
@@ -1,18 +1,33 @@
-export class Deferred<T> {
-    public readonly promise: Promise<T>
+
+export default class Deferred<T> implements PromiseLike<T> {
+    private readonly promise: Promise<T>
     private resolver?: (v?: T) => void
+    private rejecter?: (reason?: any) => void
+
     constructor() {
-        this.promise = new Promise<T>(resolve => {
+        this.promise = new Promise<T>((resolve, reject) => {
             this.resolver = resolve
+            this.rejecter = reject
         })
     }
     public resolve(v?: T) {
-        if (this.resolver) {
-            this.resolver(v)
-            return
-        }
-        process.nextTick(() => {
-            this.resolve(v)
-        })
+        this.resolver!(v)
+    }
+
+    public reject(reason?: any) {
+        this.rejecter!(reason)
+    }
+
+    public then<R1 = T, R2 = never>(
+        onfulfilled?: ((value: T) => R1 | PromiseLike<R1>) | undefined | null,
+        onrejected?: ((reason: any) => R2 | PromiseLike<R2>) | undefined | null
+    ) {
+        return this.promise.then(onfulfilled, onrejected)
+    }
+
+    public catch<R = never>(
+        onrejected?: ((reason: any) => R | PromiseLike<R>) | undefined | null
+    ) {
+        return this.promise.catch(onrejected)
     }
 }
