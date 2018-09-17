e97e1e0fcb2f73d971900608b6010b1191f1f273
Asbert Ma <stayinalive@msn.cn>
2018-09-17 09:45:11 +0800

    remove duplicate code

--- src/preload.ts
+++ src/preload.ts
@@ -23,10 +23,6 @@ const partition = webContents.getWebPreferences().partition
 
 const userAddr = partition ? new URL(partition).pathname.split('/').pop() : undefined
 
-if (process.env.NODE_ENV === 'development') {
-  remote.getCurrentWebContents().openDevTools()
-}
-
 Object.defineProperty(window, 'connex', {
     enumerable: true,
     value: remote.app.backend.connect(webContents.id, userAddr)
