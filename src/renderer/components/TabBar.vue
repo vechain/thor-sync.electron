06f75e495019bd7069e121dd2f9d8fce8965c861
Asbert Ma <stayinalive@msn.cn>
2018-09-17 16:42:16 +0800

    new tab button in tab-bar

--- src/renderer/components/TabBar.vue
+++ src/renderer/components/TabBar.vue
@@ -10,86 +10,99 @@
           <v-icon @click.stop.prevent="popClose(item.portId, item.contentId)" class="tab-close">close</v-icon>
         </div>
       </div>
+      <div class="add-tab">
+        <v-btn @click.stop="addTab({title: 'New tab'})" icon>
+          <v-icon>add</v-icon>
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator'
+import { portData } from '@/renderer/components/ViewPort.vue';
 @Component
 export default class Tabbar extends Vue {
-  @Prop() private tabs!: any[]
-  @Prop() private currentTab!: number
-  public popClose(portId: number) {
-    this.$emit('close', portId)
-  }
+    @Prop() private tabs!: any[]
+    @Prop() private currentTab!: number
 
-  public switchTab(portId: number) {
-    this.$emit('switch', portId)
-  }
+    @Emit('close')
+    public popClose(portId: number) {}
+    @Emit('switch')
+    public switchTab(portId: number) {}
+    @Emit('new-tab')
+    public addTab(data: portData) {}
 }
 </script>
 
 <style lang="scss" scoped>
 .sync-tab-bar {
-  padding-top: 10px;
-  padding-right: 5px;
-  height: 41px;
-  width: 100%;
-  -webkit-user-select: none;
-  transition: padding-left 300ms ease-in-out;
-  display: flex;
-  justify-content: space-between;
+    padding-top: 10px;
+    padding-right: 5px;
+    height: 45px;
+    width: 100%;
+    -webkit-user-select: none;
+    transition: padding-left 300ms ease-in-out;
+    display: flex;
+    justify-content: space-between;
 }
 .darwin .sync-tab-bar {
-  padding-left: 80px;
+    padding-left: 55px;
 }
 .full-screen .sync-tab-bar {
-  padding-left: 5px;
+    padding-left: 5px;
 }
 .sync-tab {
-  padding-top: 5px;
-  width: 150x;
-  height: 30px;
-  -webkit-user-select: none;
-  -webkit-app-region: no-drag;
-  // background-color: rgb(220, 226, 228);
-  border-top-left-radius: 7px;
-  border-top-right-radius: 7px;
-  border: 1px solid #eee;
-  width: 200px;
-  // background-color: #00796b;
-  cursor: normal;
+    padding-top: 8px;
+    width: 150x;
+    height: 32px;
+    -webkit-user-select: none;
+    -webkit-app-region: no-drag;
+    border-top-left-radius: 7px;
+    border-top-right-radius: 7px;
+    width: 200px;
+    background-color: #48bbc7;
+    cursor: normal;
 }
 .sync-tab.current-tab {
-  background-color: #fff;
+    background-color: rgb(154, 236, 218);
 }
 .sync-tab img {
-  width: 15px;
-  height: 15px;
-  margin-top: 2px;
+    width: 15px;
+    height: 15px;
+    margin-top: 2px;
 }
 .sync-tab span {
-  font-size: 13px;
-  max-width: 140px;
-  height: 20px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
+    font-size: 13px;
+    max-width: 140px;
+    height: 20px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 }
 .sync-tab .tab-container {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  cursor: normal;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    cursor: normal;
 }
 .tab-close {
-  color: rgba(0, 0, 0, 0.29);
-  font-size: 18px;
+    color: rgba(0, 0, 0, 0.29);
+    font-size: 18px;
 }
 .sync-tabs {
-  display: grid;
-  grid-auto-columns: 1fr;
-  grid-auto-flow: column;
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-auto-flow: column;
+}
+.sync-tabs .add-tab {
+  width: 45px;
+    // background-color: #48bbc7;
+}
+.add-tab .v-btn {
+    margin: 0;
+    height: 30px;
+    width: 30px;
 }
 </style>
--- src/renderer/components/ViewPort.vue
+++ src/renderer/components/ViewPort.vue
@@ -1,305 +1,370 @@
 <template>
-  <div v-drag="draggable" class="sync-viewport-container" :class="{'current': currentPort === instanceId, 'full-size': isFullSize, 'sync-viewport-win': draggable}">
-    <div class="port-title" v-if="draggable">
-      <span>
+    <div v-drag="draggable" class="sync-viewport-container" :class="{'current': currentPort === instanceId, 'full-size': isFullSize, 'sync-viewport-win': draggable}">
+        <div class="port-title" v-if="draggable">
+            <span>
       {{title}}
       </span>
+        </div>
+        <v-container v-if="draggable" fluid class='pa-0 ma-0 port-contral'>
+            <v-layout row>
+                <v-flex>
+                    <v-icon @click.stop="toggleFullSize">picture_in_picture_alt</v-icon>
+                </v-flex>
+                <v-flex>
+                    <v-icon @click.stop="emitCloseWin(instanceId)">close</v-icon>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <v-container fluid class='pa-0 ma-0 sync-viewport-bar'>
+            <v-layout row>
+                <v-flex>
+                </v-flex>
+                <v-flex>
+                    <v-text-field class="custom-field mt-0" height="34px" v-model="origin" :placeholder="url" @keyup.enter="emitNewTab({ url: origin })"
+                        background-color="white" prepend-inner-icon="search" single-line :hide-details="true"></v-text-field>
+                </v-flex>
+                <v-flex>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <webview v-if="url" :partition="partition" v-poster autosize :preload="preload" :src="url"></webview>
+        <!-- <DApps @open-dapp="addPort" class="default-content" :list="apps"></DApps> -->
+        <DApps @open-dapp="openDapp" v-if="!url" class="default-content" :list="apps">
+            <!-- <div class="search">
+                <v-text-field @keyup.enter="openTab" v-model="search" label="Solo" placeholder="Placeholder" solo></v-text-field>
+            </div> -->
+        </DApps>
     </div>
-    <v-container v-if="draggable" fluid class='pa-0 ma-0 port-contral'>
-      <v-layout row>
-        <v-flex>
-          <v-icon @click.stop="toggleFullSize">picture_in_picture_alt</v-icon>
-        </v-flex>
-        <v-flex>
-          <v-icon @click.stop="emitCloseWin(instanceId)">close</v-icon>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <v-container fluid class='pa-0 ma-0 sync-viewport-bar'>
-      <v-layout row>
-        <v-flex>
-        </v-flex>
-        <v-flex>
-          <v-text-field v-model="origin" :placeholder="url" @keyup.enter="emitNewTab({ url: origin })" background-color="white"
-            prepend-inner-icon="search" single-line class="mt-0" :hide-details="true"></v-text-field>
-        </v-flex>
-        <v-flex>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <webview :partition="partition" v-poster autosize :preload="preload" :src="url"></webview>
-  </div>
 </template>
 
 <script lang="ts">
 const Dragable = require('draggabilly')
+import { mapActions, mapState } from 'vuex'
 import { Component, Prop, Vue, Emit } from 'vue-property-decorator'
 import { WebviewTag } from 'electron'
 import { VNodeDirective, VNode } from 'vue'
+import DApps from './AppList.vue'
 
 export interface portData {
-  portId?: number
-  contentId?: number
-  url?: string
-  [propName: string]: any
+    portId?: number
+    contentId?: number
+    url?: string
+    [propName: string]: any
 }
 
 @Component({
-  directives: {
-    drag: {
-      unbind(ele: Element, binding: VNodeDirective, vnode: VNode) {
-        if (!binding.value) {
-          return
-        }
-        let ctx = vnode.context as any
-        let draggie = ctx.draggie
-        draggie.destroy()
-        window.removeEventListener('resize', ctx.onWindowResizeEnd)
-      },
-      bind(ele: Element, binding: VNodeDirective, vnode: VNode) {
-        if (!binding.value) {
-          return
-        }
-
-        let ctx = vnode.context as any
-
-        window.addEventListener('resize', ctx.onWindowResizeEnd)
-        let opts = {
-          containment: '.sync-container',
-          handle: '.port-title'
-        }
-        ctx.draggie = new Dragable(ele, opts)
-        ctx.draggie.setPosition(20, 20)
-        ctx.draggie.on('staticClick', function() {
-          ctx.$emit('switch-view', ctx.instanceId)
-        })
-        ctx.draggie.on('dragStart', function() {
-          ctx.$emit('switch-view', ctx.instanceId)
-        })
-      }
+    methods: {
+        ...mapActions(['getBuildInDapps'])
     },
-    poster: {
-      bind(ele: Element, binding: VNodeDirective, vnode: VNode) {
-        let ctx = vnode.context as any
-        ele.addEventListener('page-title-updated', ctx.titleUpdate)
-        ele.addEventListener('page-favicon-updated', ctx.faviconUpdate)
-        ele.addEventListener('new-window', ctx.newTab)
-        ele.addEventListener('did-start-loading', ctx.updateLoadingStatus)
-        ele.addEventListener('load-commit', ctx.updateLoadingStatus)
-        ele.addEventListener('did-finish-load', ctx.updateLoadingStatus)
-        ele.addEventListener('did-stop-loading', ctx.updateLoadingStatus)
-        ele.addEventListener('did-fail-loading', ctx.updateLoadingStatus)
-      },
-      unbind(ele: Element, binding: VNodeDirective, vnode: VNode) {
-        let ctx = vnode.context as any
-        ele.removeEventListener('new-window', ctx.newTab)
-        ele.removeEventListener('page-title-updated', ctx.titleUpdate)
-        ele.removeEventListener('page-favicon-updated', ctx.faviconUpdate)
-        ele.removeEventListener('did-start-loading', ctx.updateLoadingStatus)
-        ele.removeEventListener('load-commit', ctx.updateLoadingStatus)
-        ele.removeEventListener('did-finish-load', ctx.updateLoadingStatus)
-        ele.removeEventListener('did-stop-loading', ctx.updateLoadingStatus)
-        ele.removeEventListener('did-fail-loading', ctx.updateLoadingStatus) 
-      }
+    created() {
+        ;(this as any).getBuildInDapps().then((apps: object[]) => {
+            ;(this as any).apps = apps
+        })
+    },
+    components: {
+        DApps
+    },
+    directives: {
+        drag: {
+            unbind(ele: Element, binding: VNodeDirective, vnode: VNode) {
+                if (!binding.value) {
+                    return
+                }
+                let ctx = vnode.context as any
+                let draggie = ctx.draggie
+                draggie.destroy()
+                window.removeEventListener('resize', ctx.onWindowResizeEnd)
+            },
+            bind(ele: Element, binding: VNodeDirective, vnode: VNode) {
+                if (!binding.value) {
+                    return
+                }
+
+                let ctx = vnode.context as any
+
+                window.addEventListener('resize', ctx.onWindowResizeEnd)
+                let opts = {
+                    containment: '.v-content__wrap',
+                    handle: '.port-title'
+                }
+                ctx.draggie = new Dragable(ele, opts)
+                ctx.draggie.setPosition(20, 20)
+                ctx.draggie.on('staticClick', function() {
+                    ctx.$emit('switch-view', ctx.instanceId)
+                })
+                ctx.draggie.on('dragStart', function() {
+                    ctx.$emit('switch-view', ctx.instanceId)
+                })
+            }
+        },
+        poster: {
+            bind(ele: Element, binding: VNodeDirective, vnode: VNode) {
+                let ctx = vnode.context as any
+                ele.addEventListener('page-title-updated', ctx.titleUpdate)
+                ele.addEventListener('page-favicon-updated', ctx.faviconUpdate)
+                ele.addEventListener('new-window', ctx.newTab)
+                ele.addEventListener(
+                    'did-start-loading',
+                    ctx.updateLoadingStatus
+                )
+                ele.addEventListener('load-commit', ctx.updateLoadingStatus)
+                ele.addEventListener('did-finish-load', ctx.updateLoadingStatus)
+                ele.addEventListener(
+                    'did-stop-loading',
+                    ctx.updateLoadingStatus
+                )
+                ele.addEventListener(
+                    'did-fail-loading',
+                    ctx.updateLoadingStatus
+                )
+            },
+            unbind(ele: Element, binding: VNodeDirective, vnode: VNode) {
+                let ctx = vnode.context as any
+                ele.removeEventListener('new-window', ctx.newTab)
+                ele.removeEventListener('page-title-updated', ctx.titleUpdate)
+                ele.removeEventListener(
+                    'page-favicon-updated',
+                    ctx.faviconUpdate
+                )
+                ele.removeEventListener(
+                    'did-start-loading',
+                    ctx.updateLoadingStatus
+                )
+                ele.removeEventListener('load-commit', ctx.updateLoadingStatus)
+                ele.removeEventListener(
+                    'did-finish-load',
+                    ctx.updateLoadingStatus
+                )
+                ele.removeEventListener(
+                    'did-stop-loading',
+                    ctx.updateLoadingStatus
+                )
+                ele.removeEventListener(
+                    'did-fail-loading',
+                    ctx.updateLoadingStatus
+                )
+            }
+        }
     }
-  }
 })
 export default class ViewPort extends Vue {
-  @Prop({ default: 'https://explore.veforge.com/' })
-  private url!: string
-  @Prop({ default: Date.now() })
-  private instanceId!: number
-  @Prop() private currentPort!: number
-  @Prop({ default: false })
-  private draggable!: boolean
+    @Prop({ default: 'https://explore.veforge.com/' })
+    private url!: string
+    @Prop({ default: Date.now() })
+    private instanceId!: number
+    @Prop() private currentPort!: number
+    @Prop({ default: false })
+    private draggable!: boolean
 
-  @Emit('close')
-  emitCloseWin(id: number) {}
-  @Emit('new-tab')
-  emitNewTab(data: portData) {}
-  @Emit('favicon-updated')
-  emitFavUpdate(data: portData) {}
-  @Emit('title-updated')
-  emitTitleUpdate(data: portData) {}
-  @Emit('load-status-update')
-  emitStatusUpdate(data: portData) {}
+    apps: object[] = []
 
-  preload = window.ENV.preload
-  origin: string = ''
-  title: string = ''
+    @Emit('close')
+    emitCloseWin(id: number) {}
+    @Emit('new-tab')
+    emitNewTab(data: portData) {}
+    @Emit('favicon-updated')
+    emitFavUpdate(data: portData) {}
+    @Emit('title-updated')
+    emitTitleUpdate(data: portData) {}
+    @Emit('load-status-update')
+    emitStatusUpdate(data: portData) {}
+    @Emit('updata-url')
+    emitUrlUpdate(data: portData) {}
 
-  isFullSize: boolean = false
+    preload = window.ENV.preload
+    origin: string = ''
+    title: string = ''
 
-  draggie: any
-  winResizeEnd: any
+    isFullSize: boolean = false
 
-  get partition() {
-    let _url = new URL(this.url)
-    return `${_url.host}`
-  }
+    draggie: any
+    winResizeEnd: any
 
-  toggleFullSize() {
-    this.isFullSize = !this.isFullSize
-  }
-
-  onWindowResizeEnd(event: any) {
-    window.clearTimeout(this.winResizeEnd)
-    let _this = this
-    this.winResizeEnd = window.setTimeout(function() {
-      let position = _this.draggie.position
-      let x: number = position.x
-      let y: number = position.y
-      if (position.x > 800) {
-        x = position.x % 800
-      }
-      if (position.y > 600) {
-        y = position.y % 600
-      }
-      _this.draggie.setPosition(x, y)
-    }, 150)
-  }
-
-  updateLoadingStatus(event: Electron.Event) {
-    console.log(event)
-  }
-
-  faviconUpdate(event: Electron.PageFaviconUpdatedEvent) {
-    let wv = event.target as WebviewTag
-    let data: portData = {
-      portId: this.instanceId,
-      contentId: wv.getWebContents().id,
-      icons: event.favicons
+    get partition() {
+        if (this.url) {
+            let _url = new URL(this.url)
+            return `${_url.host}`
+        } else {
+            return ''
+        }
     }
 
-    this.emitFavUpdate(data)
-  }
-
-  titleUpdate(event: Electron.PageTitleUpdatedEvent) {
-    let wv = event.target as WebviewTag
-    let data: portData = {
-      portId: this.instanceId,
-      contentId: wv.getWebContents().id,
-      title: event.title
+    toggleFullSize() {
+        this.isFullSize = !this.isFullSize
     }
-    this.title = event.title
-    this.emitTitleUpdate(data)
-  }
 
-  newTab(evnet: Electron.NewWindowEvent) {
-    let data: portData = {
-      url: (event as any).url
+    openDapp(data: portData) {
+        let _data = {
+            portId: this.instanceId,
+            url: data.url,
+        }
+        this.emitUrlUpdate(_data)
     }
-    this.emitNewTab(data)
-  }
+
+    onWindowResizeEnd(event: any) {
+        window.clearTimeout(this.winResizeEnd)
+        let _this = this
+        this.winResizeEnd = window.setTimeout(function() {
+            let position = _this.draggie.position
+            let x: number = position.x
+            let y: number = position.y
+            if (position.x > 800) {
+                x = position.x % 800
+            }
+            if (position.y > 600) {
+                y = position.y % 600
+            }
+            _this.draggie.setPosition(x, y)
+        }, 150)
+    }
+
+    updateLoadingStatus(event: Electron.Event) {
+        console.log(event)
+    }
+
+    faviconUpdate(event: Electron.PageFaviconUpdatedEvent) {
+        let wv = event.target as WebviewTag
+        let data: portData = {
+            portId: this.instanceId,
+            contentId: wv.getWebContents().id,
+            icons: event.favicons
+        }
+
+        this.emitFavUpdate(data)
+    }
+
+    titleUpdate(event: Electron.PageTitleUpdatedEvent) {
+        let wv = event.target as WebviewTag
+        let data: portData = {
+            portId: this.instanceId,
+            contentId: wv.getWebContents().id,
+            title: event.title
+        }
+        this.title = event.title
+        this.emitTitleUpdate(data)
+    }
+
+    newTab(evnet: Electron.NewWindowEvent) {
+        let data: portData = {
+            url: (event as any).url
+        }
+        this.emitNewTab(data)
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .sync-viewport-container {
-  overflow: hidden;
-  height: 100%;
-  width: 100%;
-  transition: height 150ms linear, width 150ms linear, opacity 150ms linear;
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+    background-color: #fff;
+    transition: height 150ms linear, width 150ms linear, opacity 150ms linear;
 }
 .sync-viewport-container.sync-viewport-win {
-  max-height: 100%;
-  max-width: 100%;
-  height: 550px;
-  width: 800px;
-  border: 1px solid #eee;
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.5);
+    max-height: 100%;
+    max-width: 100%;
+    height: 550px;
+    width: 800px;
+    border: 1px solid #eee;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.5);
 }
 .sync-viewport-win.is-pointer-down {
-  z-index: 3;
-  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.3);
-  opacity: 0.7;
+    z-index: 3;
+    box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.3);
+    opacity: 0.7;
 }
 .sync-viewport-container:not(.sync-viewport-win) {
-  visibility: hidden;
+    visibility: hidden;
+    position: absolute;
+    top: 0;
 }
 .sync-viewport-container:not(.sync-viewport-win).current {
-  visibility: visible;
+    visibility: visible;
 }
 .sync-viewport-win.current {
-  resize: both;
-  z-index: 2;
-  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.3);
+    resize: both;
+    z-index: 2;
+    box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.3);
 }
 
 .sync-viewport-container.sync-viewport-win.full-size {
-  top: 0 !important;
-  left: 0 !important;
-  width: 100% !important;
-  height: 100% !important;
-  border: none;
-  border-radius: 0;
-  box-shadow: none;
-  resize: none;
+    top: 0 !important;
+    left: 0 !important;
+    width: 100% !important;
+    height: 100% !important;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+    resize: none;
 }
 .sync-viewport-win::-webkit-resizer {
-  background-image: -webkit-gradient(
-    linear,
-    right bottom,
-    right top,
-    color-stop(0, #d8d8d8),
-    color-stop(0.83, #afafaf)
-  );
+    background-image: -webkit-gradient(
+        linear,
+        right bottom,
+        right top,
+        color-stop(0, #d8d8d8),
+        color-stop(0.83, #afafaf)
+    );
 }
 .sync-viewport-win:after {
-  content: ' ';
-  display: block;
-  width: 10px;
-  height: 10px;
-  background-color: transparent;
-  position: absolute;
-  bottom: 0;
-  border-top-left-radius: 10px;
-  right: 0;
+    content: ' ';
+    display: block;
+    width: 10px;
+    height: 10px;
+    background-color: transparent;
+    position: absolute;
+    bottom: 0;
+    border-top-left-radius: 10px;
+    right: 0;
 }
 .sync-viewport-win.current:not(.full-size):hover:after {
-  cursor: nwse-resize;
-  background-color: rgba(0, 0, 0, 0.3);
+    cursor: nwse-resize;
+    background-color: rgba(0, 0, 0, 0.3);
 }
 .sync-viewport-win .port-contral {
-  width: 50px;
-  height: 25px;
-  position: absolute;
-  top: 0;
-  right: 0;
+    width: 50px;
+    height: 25px;
+    position: absolute;
+    top: 0;
+    right: 0;
 }
 .sync-viewport-win .port-contral .v-icon {
-  font-size: 19px;
-  line-height: 20px;
+    font-size: 19px;
+    line-height: 20px;
 }
 .sync-viewport-win .port-title {
-  height: 25px;
-  left: 20px;
-  text-align: center;
-  font-size: 14px;
-  line-height: 25px;
-  color: #424242;
-  border-bottom: 1px solid #eee;
-  background-image: -webkit-gradient(
-    linear,
-    right bottom,
-    right top,
-    color-stop(0, #d8d8d8),
-    color-stop(0.83, #afafaf)
-  );
+    height: 25px;
+    left: 20px;
+    text-align: center;
+    font-size: 14px;
+    line-height: 25px;
+    color: #424242;
+    border-bottom: 1px solid #eee;
+    background-image: -webkit-gradient(
+        linear,
+        right bottom,
+        right top,
+        color-stop(0, #d8d8d8),
+        color-stop(0.83, #afafaf)
+    );
 }
 .sync-viewport-win .port-title:hover {
-  cursor: move;
+    cursor: move;
 }
 .sync-viewport-container webview {
-  background-color: #fff;
-  height: calc(100% - 35px);
+    background-color: #fff;
+    height: calc(100% - 35px);
 }
 
 .sync-viewport-bar {
-  height: 35px;
-  width: 100%;
-  background-color: #cfd8dc;
+    height: 35px;
+    width: 100%;
+    background-color: #cfd8dc;
+}
+.custom-field {
+    padding-top: 0;
 }
 </style>
