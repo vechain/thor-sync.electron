bbc140219f942065112a76510cac86a3ee946475
qianbin <cola.tin.com@gmail.com>
2018-09-19 11:01:36 +0800

    cleanup codes

--- src/renderer/window.init.ts
+++ src/renderer/window.init.ts
@@ -1,29 +1,20 @@
-import env from '@/env'
-import Wallet from './wallet'
 import { remote } from 'electron'
-
 const win = remote.getCurrentWindow()
 const style = 'full-screen'
 const bodyEle = document.body
 const platform = process.platform
 
 declare interface ILib {
-    DApps?: string[]
     sign?(contentId: number, clause?: object[]): Promise<string>
 }
 
 declare global {
     interface Window {
-        ENV: typeof env
-        walletStore: Wallet.Store
         Lib: ILib
     }
 }
 
-window.ENV = env
-window.walletStore = new Wallet.Store()
 window.Lib = {
-    DApps: remote.app.DApps
 }
 
 function init() {
