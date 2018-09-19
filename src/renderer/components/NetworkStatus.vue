a2875693085bffcf9d49264cb368882b8064fbac
qianbin <cola.tin.com@gmail.com>
2018-09-19 20:30:26 +0800

    add NetworkStatus component

---
+++ src/renderer/components/NetworkStatus.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-chip :color="syncProgress === 1 ? 'green' : 'red'" disabled text-color="white" small>
+        {{network}}
+    </v-chip>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { remote } from 'electron'
+import Deferred from '@/base/deferred'
+import { setInterval, clearInterval } from 'timers';
+
+@Component
+export default class NetworkStatus extends Vue {
+    network = remote
+        .app
+        .backend
+        .getSiteConfig(remote.getCurrentWebContents().id)!.name
+
+    syncProgress = thor.syncProgress
+
+    created() {
+        const timer = setInterval(() => {
+            this.syncProgress = thor.syncProgress
+        }, 5000)
+
+        this.beforeDestroy = () => {
+            clearInterval(timer)
+        }
+    }
+}
+</script>
