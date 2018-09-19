6ea3e15b74d86dea5877dd0e3e48e37fc2789c52
qianbin <cola.tin.com@gmail.com>
2018-09-20 01:57:23 +0800

    add lazy promise; refactor Deferred

--- src/base/deferred.ts
+++ src/base/deferred.ts
@@ -1,33 +1,81 @@
 
-export default class Deferred<T> implements PromiseLike<T> {
-    private readonly promise: Promise<T>
-    private resolver?: (v?: T) => void
-    private rejecter?: (reason?: any) => void
-
+class Deferred<T> extends Promise<T> {
     constructor() {
-        this.promise = new Promise<T>((resolve, reject) => {
-            this.resolver = resolve
-            this.rejecter = reject
+        if (arguments.length > 0) {
+            // fallback to Promise constructor
+            super(arguments[0])
+            this.resolve = () => { throw new Error('Deferred.resolve is not callable') }
+            this.reject = () => { throw new Error('Deferred.reject is not callable') }
+            return
+        }
+
+        let _resolve: any
+        let _reject: any
+        super((resolve, reject) => {
+            _resolve = resolve
+            _reject = reject
         })
-    }
-    public resolve(v?: T) {
-        this.resolver!(v)
-    }
 
-    public reject(reason?: any) {
-        this.rejecter!(reason)
-    }
-
-    public then<R1 = T, R2 = never>(
-        onfulfilled?: ((value: T) => R1 | PromiseLike<R1>) | undefined | null,
-        onrejected?: ((reason: any) => R2 | PromiseLike<R2>) | undefined | null
-    ) {
-        return this.promise.then(onfulfilled, onrejected)
-    }
-
-    public catch<R = never>(
-        onrejected?: ((reason: any) => R | PromiseLike<R>) | undefined | null
-    ) {
-        return this.promise.catch(onrejected)
+        this.resolve = _resolve
+        this.reject = _reject
     }
 }
+
+interface Deferred<T> {
+    resolve(v?: T | PromiseLike<T>): void
+    reject(reason?: any): void
+}
+
+
+let _lazyCallingThen = false
+
+export class Lazy<T> extends Promise<T> {
+    constructor(
+        executor: (
+            resolve: (value?: T | PromiseLike<T>) => void,
+            reject: (reason?: any) => void) => void
+    ) {
+        if (_lazyCallingThen) {
+            // fallback to Promise constructor
+            super(executor)
+            return
+        }
+
+        let delayed: () => void
+        super((resolve, reject) => {
+            delayed = () => {
+                if (executor) {
+                    try {
+                        executor(resolve, reject)
+                    } finally {
+                        executor = undefined as any
+                    }
+                }
+            }
+        })
+
+        const _then = super.then
+        this.then = function() {
+            setImmediate(delayed)
+            try {
+                _lazyCallingThen = true
+                return _then.apply(this, arguments)
+            } finally {
+                _lazyCallingThen = false
+            }
+        }
+
+        const _catch = super.catch
+        this.catch = function() {
+            setImmediate(delayed)
+            try {
+                _lazyCallingThen = true
+                return _catch.apply(this, arguments)
+            } finally {
+                _lazyCallingThen = false
+            }
+        }
+    }
+}
+
+export default Deferred
