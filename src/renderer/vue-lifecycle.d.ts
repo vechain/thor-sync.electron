685e5670bdf9d06617cfd44b33ccdfe87b58e6f2
qianbin <cola.tin.com@gmail.com>
2018-09-19 20:28:33 +0800

    add Vue lifecycle declaration

---
+++ src/renderer/vue-lifecycle.d.ts
@@ -0,0 +1,15 @@
+import { Vue } from 'vue-property-decorator'
+
+// declare vue life-cycle hooks
+declare module 'vue/types/vue' {
+    export interface Vue {
+        beforeCreate(): void
+        created(): void
+        beforeMount(): void
+        mounted(): void
+        beforeUpdate(): void
+        updated(): void
+        beforeDestroy(): void
+        destroyed(): void
+    }
+}
