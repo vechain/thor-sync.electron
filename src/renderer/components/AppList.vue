09823c8ffd10d4b867d7f2e6d9fd018418fdd486
Asbert Ma <stayinalive@msn.cn>
2018-09-20 16:54:40 +0800

    select account when open dapp

--- src/renderer/components/AppList.vue
+++ src/renderer/components/AppList.vue
@@ -8,6 +8,19 @@
                     </v-img>
                     <v-card-actions>
                         <v-spacer></v-spacer>
+                        <!-- <v-menu full-width offset-y origin="center center" transition="scale-transition">
+                            <v-btn slot="activator" @click="getAccounts">
+                                {{item.name}}
+                            </v-btn>
+                            <v-list>
+                                <v-list-tile v-for="(account, i) in accounts" :key="i" @click="openDapp({src: item.src, name: item.name, addressBar: item.needAddress, account: account.address})">
+                                    <v-list-tile-content>
+                                        <v-list-tile-title>{{ account.name }}</v-list-tile-title>
+                                        <v-list-tile-sub-title>{{ account.address }}</v-list-tile-sub-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </v-list>
+                        </v-menu> -->
                         <v-btn @click.stop="openDapp({src: item.src, name: item.name, addressBar: item.needAddress})" flat>{{item.name}}</v-btn>
                     </v-card-actions>
                 </v-card>
@@ -21,7 +34,22 @@ import { Vue, Prop, Component, Emit } from 'vue-property-decorator'
 
 @Component
 export default class DApps extends Vue {
-    private apps = !window.ENV.devMode ? ['insight', 'wallet'] : ['insight', 'wallet', 'api']
+    private apps = !window.ENV.devMode
+        ? ['insight', 'wallet']
+        : ['insight', 'wallet', 'api']
+
+    // private accounts: any[] = []
+
+    // public getAccounts() {
+    //     walletStore.list().then(list => {
+    //         this.accounts = list.map(item => {
+    //             return {
+    //                 address: item.address,
+    //                 name: item.name
+    //             }
+    //         })
+    //     })
+    // }
 
     get list() {
         return this.apps.map<Dapp.Item>(item => {
@@ -34,23 +62,23 @@ export default class DApps extends Vue {
     }
 
     @Emit('open-dapp')
-    openDapp(data: Dapp.Item) { }
+    openDapp(data: Dapp.Item) {}
 }
 </script>
 
 <style lang="scss" scoped>
 .sync-dapp-list div.dapp-container {
-  display: grid;
-  grid-template-columns: 160px 160px 160px 160px 160px 160px;
-  grid-column-gap: 20px;
-  grid-row-gap: 30px;
-  justify-content: center;
+    display: grid;
+    grid-template-columns: 160px 160px 160px 160px 160px 160px;
+    grid-column-gap: 20px;
+    grid-row-gap: 30px;
+    justify-content: center;
 }
 
 @media screen and (max-width: 1200px) {
-  .sync-dapp-list div.dapp-container {
-    grid-template-columns: 160px 160px 160px 160px;
-  }
+    .sync-dapp-list div.dapp-container {
+        grid-template-columns: 160px 160px 160px 160px;
+    }
 }
 </style>
 
