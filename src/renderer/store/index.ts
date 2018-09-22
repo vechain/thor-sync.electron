0f0c956177f24214b827ad3e1407e760a4862768
qianbin <cola.tin.com@gmail.com>
2018-09-23 01:22:05 +0800

    add chain status into root store

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -1,13 +1,38 @@
 import Vuex from 'vuex'
 import WalletStore from './wallets'
 
-export default class RootStore extends Vuex.Store<{}> {
+namespace RootStore {
+    export type Model = {
+        chainStatus: Connex.Thor.Status
+    }
+}
+
+class RootStore extends Vuex.Store<RootStore.Model> {
     constructor() {
         super({
-            state: {},
+            state: {
+                chainStatus: THOR.status
+            },
+            mutations: {
+                updateStatus(state) {
+                    state.chainStatus = THOR.status
+                }
+            },
             modules: {
                 wallets: new WalletStore()
             }
-        })
+        });
+
+        (async () => {
+            for (; ;) {
+                await THOR.nextTick()
+                this.commit('updateStatus')
+            }
+        })()
+        setInterval(() => {
+            this.commit('updateStatus')
+        }, 60 * 1000)
     }
 }
+
+export default RootStore
