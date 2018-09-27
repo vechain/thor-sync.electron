b0e0a8ab2cb1736fe811e92737ed1aa3ab506425
Asbert Ma <stayinalive@msn.cn>
2018-09-27 14:31:43 +0800

    breakpoints class remove

--- src/dapps/api/App.vue
+++ src/dapps/api/App.vue
@@ -6,7 +6,7 @@
         <v-toolbar color="blue-grey lighten-1" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
             <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
                 <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-                <span class="hidden-sm-and-down">Api Demo</span>
+                <span>Api Demo</span>
             </v-toolbar-title>
         </v-toolbar>
         <v-content>
