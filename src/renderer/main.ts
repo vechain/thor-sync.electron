6219fa90371b63c296ba1b74339b6b9989e48c11
qianbin <cola.tin.com@gmail.com>
2018-09-27 22:59:51 +0800

    add more test wallet

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -51,36 +51,6 @@ Object.defineProperty(window, 'THOR', {
     enumerable: true
 })
 
-
-if (ENV.devMode) {
-    WALLETS.save({
-        address: '0xf6e78a5584c06e2dec5c675d357f050a5402a730',
-        name: 'Test',
-        keystore: {
-            address: 'f6e78a5584c06e2dec5c675d357f050a5402a730',
-            crypto: {
-                cipher: 'aes-128-ctr',
-                cipherparams: { iv: 'ddbdf9ccf12446fb15a5252d27ff46ae' },
-                ciphertext:
-                    '637cc65de2515c4d9e52b60ffd944de79b5b2adedc322a8d713daf32c592ae53',
-                kdf: 'scrypt',
-                kdfparams: {
-                    dklen: 32,
-                    n: 262144,
-                    p: 1,
-                    r: 8,
-                    salt:
-                        '1449a3cbaf300a030a4f6e7e60dcce9367a3fee5ee9079d7a363c5972bdb14b6'
-                },
-                mac:
-                    'cba0573d099c976213900d4751e8b96eb622c58dc0c2e9b37860db95aeb35081'
-            },
-            id: 'e4e54c43-3935-4541-8e73-66d3C289842F',
-            version: 3
-        }
-    }, true)
-}
-
 // the portal root
 new Nova({ store: new Store() }).$mount('#nova')
 // user interaction proxy root
