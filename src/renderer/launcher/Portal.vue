7762559711961a79f0c85175b9cba33c01020dfa
qianbin <cola.tin.com@gmail.com>
2018-10-01 21:39:58 +0800

    add simple portal and wallets page

---
+++ src/renderer/launcher/Portal.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        This is the portal
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+@Component
+export default class Portal extends Vue {
+   
+}
+</script>
