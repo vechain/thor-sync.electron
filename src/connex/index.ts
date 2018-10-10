1164bfb20d43b97c544ceb8eaa175ff2356e7d48
qianbin <cola.tin.com@gmail.com>
2018-10-11 00:00:16 +0800

    tweak connex interface 1. remove user prop 2. move sign method into vendor 3. add site config

--- src/connex/index.ts
+++ src/connex/index.ts
@@ -1,17 +1,11 @@
 import { create as createThor } from './thor'
 
 export function create(
-    user: Connex.User | undefined,
     site: Connex.Thor.Site,
     vendor: Connex.Vendor,
 ): Connex {
     const thor = createThor(site)
     return {
-        get user() {
-            if (user) {
-                return { ...user }
-            }
-        },
         get thor() { return thor },
         get vendor() { return vendor }
     }
