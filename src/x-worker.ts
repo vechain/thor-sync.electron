fdd2ae06362de71145be136fcd737be75cb06542
qianbin <cola.tin.com@gmail.com>
2018-10-11 00:09:40 +0800

    update preload, x-worker

--- src/x-worker.ts
+++ src/x-worker.ts
@@ -10,17 +10,19 @@ declare module 'electron' {
     }
 }
 
-remote.app.inject('xWorker', {
-    maximizeWindow: (id: number, cb: () => void) => {
-        remote.BrowserWindow.fromId(id).maximize()
-        cb()
-    },
-    unmaximizeWindow: (id: number, cb: () => void) => {
-        remote.BrowserWindow.fromId(id).unmaximize()
-        cb()
-    },
-    minimizeWindow: (id: number, cb: () => void) => {
-        remote.BrowserWindow.fromId(id).minimize()
-        cb()
-    }
-})
+remote.app.EXTENSION.inject(
+    remote.getCurrentWebContents().id,
+    'xWorker', {
+        maximizeWindow: (id: number, cb: () => void) => {
+            remote.BrowserWindow.fromId(id).maximize()
+            cb()
+        },
+        unmaximizeWindow: (id: number, cb: () => void) => {
+            remote.BrowserWindow.fromId(id).unmaximize()
+            cb()
+        },
+        minimizeWindow: (id: number, cb: () => void) => {
+            remote.BrowserWindow.fromId(id).minimize()
+            cb()
+        }
+    })
