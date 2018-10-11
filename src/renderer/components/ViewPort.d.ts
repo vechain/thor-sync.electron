4cda955264915bb3a0b89a1addd2266fd4a0cea3
Asbert Ma <stayinalive@msn.cn>
2018-10-11 18:05:00 +0800

    search bar in viewport

--- src/renderer/components/ViewPort.d.ts
+++ src/renderer/components/ViewPort.d.ts
@@ -1,10 +1,14 @@
 declare namespace ViewPort {
     type DataUpdateEvent = {
-        type: string | 'title' | 'icon' | 'url' | 'new-window',
+        type: string, // | 'title' | 'icon' | 'url' | 'new-window',
         value: string | number
     }
 
     type StatusUpdateEvent = {
         status: string
+    }
+
+    type Opt = {
+        src: string
     }
 }
