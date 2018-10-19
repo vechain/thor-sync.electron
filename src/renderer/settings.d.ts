5e4e6962fc98a8729e6c98087ee87f7c97832e83
Asbert Ma <stayinalive@msn.cn>
2018-10-19 16:55:45 +0800

    add setting's types

--- src/renderer/settings.d.ts
+++ src/renderer/settings.d.ts
@@ -1,12 +1,12 @@
 declare namespace Settings {
     export type NetWork = {
         name: string
-        domain: string
+        type: string
+        rpcurl: string
     }
 
     export type Shortcut = {
         name: string
-        type: string
-        source: string
+        domain: string
     }
 }
