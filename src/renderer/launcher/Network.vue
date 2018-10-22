64b7902906a7e54f480dfab2a87a2ffbccba47de
Asbert Ma <stayinalive@msn.cn>
2018-10-22 17:38:22 +0800

    add settings list

--- src/renderer/launcher/Network.vue
+++ src/renderer/launcher/Network.vue
@@ -5,21 +5,38 @@
                 <v-toolbar>
                     <v-toolbar-title>Networks</v-toolbar-title>
                     <v-divider class="mx-3" inset vertical></v-divider>
-                    <span>
-                        SYNC already built-in networks for you, you can also add a custom RPC URL on your own
-                    </span>
-                    <v-spacer />
-                    <NewNetworkDialog />
+                    <span>SYNC already built-in networks for you, you can also add a custom RPC URL on your own</span>
+                    <v-spacer/>
+                    <NewNetworkDialog
+                        @cancel="onCancelEdit"
+                        @updated="onUpdate"
+                        :editItem="editItem"
+                        v-model="dialog"
+                    />
                 </v-toolbar>
                 <v-data-table width="800px" :headers="headers" :items="list">
                     <template slot="items" slot-scope="props">
                         <td class="text-xs-center">{{props.item.value.name}}</td>
-                        <td class="text-xs-center">{{props.item.value.type}}</td>
                         <td class="text-xs-center">{{props.item.value.rpcurl}}</td>
+                        <td class="text-xs-center">
+                            <v-btn
+                                dark
+                                small
+                                icon
+                                color="blue-grey lighten-3"
+                                @click="onEdit(props.item)"
+                            >
+                                <v-icon small>edit</v-icon>
+                            </v-btn>
+                            <v-btn @click="onDelete(props.item)" dark small icon color="blue-grey lighten-3">
+                                <v-icon small>delete</v-icon>
+                            </v-btn>
+                        </td>
                     </template>
                 </v-data-table>
             </v-flex>
         </v-layout>
+        <ConfirmDialog max-width="300px" ref="confirm"/>
     </v-container>
 </template>
 <script lang="ts">
@@ -28,26 +45,23 @@ import { State } from 'vuex-class'
 import Preferences from '@/renderer/preferences'
 import { Entities } from '../database'
 import NewNetworkDialog from './NewNetworkDialog.vue'
+import ConfirmDialog from '../components/ConfirmDialog.vue'
 
 @Component({
     components: {
-        NewNetworkDialog
+        NewNetworkDialog,
+        ConfirmDialog
     }
 })
 export default class Networks extends Vue {
     name = 'networks'
     dialog = false
+    editItem: Entities.Network | null = null
     list: Entities.Network[] = []
     headers = [
         {
             text: 'Name',
             value: 'name',
-            align: 'center',
-            sortable: false
-        },
-        {
-            text: 'Type',
-            value: 'domain',
             align: 'center',
             sortable: false
         },
@@ -68,17 +82,44 @@ export default class Networks extends Vue {
     created() {
         this.loadList()
     }
-
     @State
     networksRevision!: number
 
     @Watch('networksRevision')
     async loadList() {
         this.list = await DB.preferences
-            .filter(item => {
-                return item.key === 'network'
-            })
-            .toArray()
+            .where('key')
+            .equals('network')
+            .reverse()
+            .sortBy('id')
+    }
+
+    onEdit(network: Entities.Network) {
+        this.editItem = network
+        this.dialog = true
+    }
+
+    async onDelete(item: Entities.Network) {
+        let conDig = this.$refs.confirm as ConfirmDialog
+        conDig.confirm().then(
+            () => {
+                return DB.preferences.delete(item.id as any)
+            },
+            () => {
+                console.log('cancel')
+            }
+        )
+    }
+
+    onUpdate(network: Entities.Network) {
+        const index = this.list.findIndex(item => {
+            return item.id === network.id
+        })
+
+        this.$set(this.list, index, network)
+    }
+    onCancelEdit() {
+        this.editItem = null
     }
 }
 </script>
--- src/renderer/launcher/Shortcut.vue
+++ src/renderer/launcher/Shortcut.vue
@@ -5,21 +5,44 @@
                 <v-toolbar>
                     <v-toolbar-title>Shortcuts</v-toolbar-title>
                     <v-divider class="mx-3" inset vertical></v-divider>
-                    <span>
-                        Your favourite DApp will appear below
-                    </span>
-                    <v-spacer />
-                    <NewShortcutDialog />
+                    <span>Your favourite DApp will appear below</span>
+                    <v-spacer/>
+                    <NewShortcutDialog
+                        @cancel="onCancelEdit"
+                        @updated="onUpdate"
+                        :editItem="editItem"
+                        v-model="dialog"
+                    />
                 </v-toolbar>
                 <v-data-table width="800px" :headers="headers" :items="shortcuts">
                     <template slot="items" slot-scope="props">
                         <td class="text-xs-center">{{props.item.value.name}}</td>
                         <td class="text-xs-center">{{props.item.value.domain}}</td>
-                        <td></td>
+                        <td class="text-xs-center">
+                            <v-btn
+                                dark
+                                small
+                                icon
+                                color="blue-grey lighten-3"
+                                @click="onEdit(props.item)"
+                            >
+                                <v-icon small>edit</v-icon>
+                            </v-btn>
+                            <v-btn
+                                @click="onDelete(props.item)"
+                                dark
+                                small
+                                icon
+                                color="blue-grey lighten-3"
+                            >
+                                <v-icon small>delete</v-icon>
+                            </v-btn>
+                        </td>
                     </template>
                 </v-data-table>
             </v-flex>
         </v-layout>
+        <ConfirmDialog max-width="300px" ref="confirm"/>
     </v-container>
 </template>
 <script lang="ts">
@@ -28,15 +51,18 @@ import { State } from 'vuex-class'
 import Preferences from '@/renderer/preferences'
 import { Entities } from '../database'
 import NewShortcutDialog from './NewShortcutDialog.vue'
+import ConfirmDialog from '../components/ConfirmDialog.vue'
 
 @Component({
     components: {
-        NewShortcutDialog
+        NewShortcutDialog,
+        ConfirmDialog
     }
 })
 export default class Shortcuts extends Vue {
     name = 'shortcuts'
     dialog = false
+    editItem: Entities.Shortcut | null = null
     shortcuts: Entities.Shortcut[] = []
     headers = [
         {
@@ -63,13 +89,44 @@ export default class Shortcuts extends Vue {
         this.loadList()
     }
 
-    @State shortcutsRevision!: number
+    onEdit(val: Entities.Shortcut) {
+        this.editItem = val
+        this.dialog = true
+    }
+
+    onUpdate(shortcut: Entities.Shortcut) {
+        const index = this.shortcuts.findIndex(item => {
+            return item.id === shortcut.id
+        })
+
+        this.$set(this.shortcuts, index, shortcut)
+    }
+    onCancelEdit() {
+        this.editItem = null
+    }
+
+    async onDelete(item: Entities.Shortcut) {
+        let conDig = this.$refs.confirm as ConfirmDialog
+        conDig.confirm().then(
+            () => {
+                return DB.preferences.delete(item.id as any)
+            },
+            () => {
+                console.log('cancel')
+            }
+        )
+    }
+
+    @State
+    shortcutsRevision!: number
 
     @Watch('shortcutsRevision')
     async loadList() {
-        this.shortcuts = await DB.preferences.filter((item) => {
-            return item.key === 'shortcut'
-        }).toArray()
+        this.shortcuts = await DB.preferences
+            .where('key')
+            .equals('shortcut')
+            .reverse()
+            .sortBy('id')
     }
 }
 </script>
