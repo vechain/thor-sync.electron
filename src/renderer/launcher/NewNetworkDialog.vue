c63428ae1aa1897c55ccdbc8766e60940d956e42
Asbert Ma <stayinalive@msn.cn>
2018-10-22 17:38:52 +0800

    new item dialog

--- src/renderer/launcher/NewNetworkDialog.vue
+++ src/renderer/launcher/NewNetworkDialog.vue
@@ -1,19 +1,24 @@
 <template>
-    <v-dialog persistent v-model="dialog" max-width="500px">
+    <v-dialog v-bind="$attrs" persistent v-model="dialog" max-width="500px">
         <v-btn slot="activator" color="primary darken-1" class="mb-2">New Item</v-btn>
         <v-card>
             <v-card-text>
-                <div class="headline">New Network</div>
+                <div class="headline">{{isEditing ? 'Edit Network' : 'New Network'}}</div>
                 <v-layout>
                     <v-flex>
                         <v-form ref="form" v-model="valid">
-                            <v-text-field v-model="form.name" :rules="rules.name" label="Name"
-                                required></v-text-field>
-                            <v-select :items="types" :rules="rules.type" label="Type" v-model="form.type"
-                                item-text="key" item-value="value">
-                            </v-select>
-                            <v-text-field :rules="rules.rpcUrl" v-model="form.rpcUrl" label="RPC URL"
-                                required></v-text-field>
+                            <v-text-field
+                                v-model="form.name"
+                                :rules="rules.name"
+                                label="Name"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                :rules="rules.rpcUrl"
+                                v-model="form.rpcUrl"
+                                label="RPC URL"
+                                required
+                            ></v-text-field>
                         </v-form>
                     </v-flex>
                 </v-layout>
@@ -28,49 +33,99 @@
 </template>
 <script lang="ts">
 import { Entities } from '../database'
-import { Vue, Component } from 'vue-property-decorator'
+import {
+    Vue,
+    Component,
+    Model,
+    Watch,
+    Prop,
+    Emit
+} from 'vue-property-decorator'
+import Preferences from '@/renderer/preferences'
+import { dialog } from 'electron'
 
 @Component
 export default class NewNetworkDialog extends Vue {
     name = 'NewNetworkDialog'
+    isEditing = false
+    @Model('input')
+    value!: boolean
+
+    @Prop()
+    editItem!: Entities.Network | null
+
     dialog = false
     valid = true
-    types = [
-        {
-            key: 'Mainnet',
-            value: 'main'
-        },
-        {
-            key: 'Testnet',
-            value: 'testnet'
-        }
-    ]
     form = {
         name: '',
-        rpcUrl: '',
-        type: ''
+        rpcUrl: ''
     }
     rules = {
         name: [v => !!v || 'Name is required'],
-        rpcUrl: [v => !!v || 'RPC URL is required'],
-        type: [v => !!v || 'Type is required']
+        rpcUrl: [v => !!v || 'RPC URL is required']
     }
+
+    @Watch('value')
+    valueChanged(val: boolean) {
+        this.dialog = val
+
+        if (this.editItem) {
+            this.form.name = this.editItem!.value.name
+            this.form.rpcUrl = this.editItem!.value.rpcurl
+        }
+        this.isEditing = !!this.editItem
+    }
+
+    @Watch('dialog')
+    valueUpdate(val: boolean) {
+        this.dialogChanged(val)
+    }
+    @Emit('input')
+    dialogChanged(val: boolean) {}
+
+    @Emit('updated')
+    onEdited(editItem: Entities.Network) {}
+
+    @Emit('cancel')
+    onCancel() {}
 
     clear() {
         this.dialog = false
-        this.$refs.form.reset()
+        let form = this.$refs.form as any
+        form.reset()
+        this.onCancel()
     }
     save() {
-        if (this.$refs.form.validate()) {
-            this.dialog = false
-
-            DB.preferences.add(Entities.Network.create({
-                name: this.form.name,
-                type: this.form.type,
-                rpcurl: this.form.rpcUrl
-            })).then(() => {
-                this.clear()
-            })
+        let form = this.$refs.form as any
+        if (!form.validate()) {
+            return
+        }
+        if (this.isEditing) {
+            let result = Object.assign({}, this.editItem)
+            result.value.name = this.form.name
+            result.value.rpcurl = this.form.rpcUrl
+            DB.preferences
+                .update(this.editItem!.id as any, { value: result.value })
+                .then(updated => {
+                    if (updated) {
+                        this.isEditing = false
+                        this.onEdited(result)
+                        this.clear()
+                    } else {
+                        console.error('Edit newwork failed')
+                    }
+                })
+        } else {
+            DB.preferences
+                .add(
+                    Entities.Network.create({
+                        name: this.form.name,
+                        rpcurl: this.form.rpcUrl
+                    })
+                )
+                .then(() => {
+                    this.clear()
+                })
         }
     }
 }
