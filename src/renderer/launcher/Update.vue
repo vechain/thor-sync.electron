5bb050bc204882aca276daf52f67d0064083ec8f
Asbert Ma <stayinalive@msn.cn>
2018-10-23 13:54:46 +0800

    auto update setting

--- src/renderer/launcher/Update.vue
+++ src/renderer/launcher/Update.vue
@@ -1,5 +1,72 @@
 <template>
-    <div>
-        Update
-    </div>
+    <v-container>
+        <v-layout row justify-center>
+            <v-flex sm12 lg6>
+                <v-card>
+                    <v-card-title class="headline">Setting</v-card-title>
+                    <v-card-text
+                        class="grey--text text--darken-1"
+                    >"Sync" releases updates from time to time. Install the update version for feature, security fixes, and improved software stability.</v-card-text>
+                    <v-list>
+                        <v-list-tile>
+                            <v-list-tile-content>
+                                <v-list-tile-title class="grey--text text--darken-3">Auto Update</v-list-tile-title>
+                            </v-list-tile-content>
+                            <!-- <v-list-tile-content> -->
+                            <v-list-tile-action>
+                                <v-switch @change="onChange" v-model="isAuto.value"></v-switch>
+                            </v-list-tile-action>
+                            <!-- </v-list-tile-content> -->
+                        </v-list-tile>
+                        <v-list-tile>
+                            <v-list-tile-content>
+                                <v-list-tile-title
+                                    class="grey--text text--darken-3"
+                                >Current Software Version</v-list-tile-title>
+                            </v-list-tile-content>
+                            <v-list-tile-action-text
+                                class="grey--text text--darken-3"
+                            >Current Software Version</v-list-tile-action-text>
+                        </v-list-tile>
+                    </v-list>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator'
+import { State } from 'vuex-class'
+import { Entities } from '../database'
+import Preferences from '@/renderer/preferences'
+
+@Component
+export default class AutoUpdate extends Vue {
+    name = 'auto_update'
+    isAuto: any = {
+        key: Preferences.KEY_IS_AUTO_UPDATE,
+        value: false
+    }
+    @State
+    preferenceRevision!: number
+
+    async created() {
+        await this.getConfig()
+    }
+
+    onChange() {
+        DB.preferences.update(this.isAuto.id as any, {
+            value: this.isAuto.value
+        })
+    }
+
+    @Watch('preferenceRevision')
+    async getConfig() {
+        this.isAuto = await DB.preferences
+            .where('key')
+            .equals(Preferences.KEY_IS_AUTO_UPDATE)
+            .first()
+    }
+}
+</script>
+
