a679272395b686d5888ed2d545454fbfd1078419
Asbert Ma <stayinalive@msn.cn>
2018-10-26 13:52:51 +0800

    qrcode dialog

---
+++ src/renderer/components/QRCodeDialog.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-dialog origin="bottom right" v-bind="$attrs">
+        <v-btn slot="activator" large icon >
+            <v-icon>mdi-qrcode</v-icon>
+        </v-btn>
+        <v-card>
+            <v-card-text>
+                <QRcode :pre="pre" :size="size">{{content}}</QRcode>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+<script lang="ts">
+import QRcode from './QRcode.vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+@Component({
+    components: {
+        QRcode
+    }
+})
+export default class QRCodeDialog extends Vue {
+    name = 'QRCodeDialog'
+    @Prop(String)
+    content!: string
+    @Prop(Number)
+    size!: number
+    @Prop({default: false})
+    pre!: boolean
+}
+</script>
+
