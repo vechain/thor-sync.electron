4315b1e6ab3cf6fa802fb71f3723bbaeeef62f5e
qianbin <cola.tin.com@gmail.com>
2018-11-28 19:50:36 +0800

    improve app button

--- src/renderer/components/AppButton.vue
+++ src/renderer/components/AppButton.vue
@@ -9,48 +9,26 @@
     >
         <v-layout column align-center>
             <v-layout
-                style="width:56px;height:56px;overflow:hidden;position:relative;border-radius:6px 6px 6px 6px"
+                style="width:56px;height:56px;position:relative;border-radius:7px;"
                 class="ma-2"
                 align-center
                 justify-center
                 :style="{'background-color':faceColor}"
             >
                 <span class="white--text headline font-weight-light">{{firstChar}}</span>
-                <div
-                    v-show="loaded"
-                    style="position:absolute;width:19px;height:19px;top:0px;right:0px;border-radius:0px 6px 0px 0px"
-                >
-                    <div
-                        class="white"
-                        style="position:absolute;width:3px;height:3px;left:0px;top:0px"
-                    >
-                        <div
-                            style="width:100%;height:100%;border-radius:0px 3px 0px 0px"
-                            :style="{'background-color':faceColor}"
-                        />
-                    </div>
-                    <div
-                        class="white"
-                        style="position:absolute;width:3px;height:3px;right:0px;bottom:0px"
-                    >
-                        <div
-                            style="width:100%;height:100%;border-radius:0px 3px 0px 0px"
-                            :style="{'background-color':faceColor}"
-                        />
-                    </div>
+                <div v-show="loaded" class="badge">
                     <img
-                        class="white"
                         :src="favicon"
                         width="16px"
                         height="16px"
-                        style="position:absolute;top:0px;right:0px;border-radius:0px 6px 0px 6px;overflow:hidden"
+                        style="position:absolute;top:1px;right:1px;"
                         @load="onImageLoad"
                     >
+                    <div
+                        style="position:absolute;top:0px;right:0px;left:-2px;bottom:-2px;border-radius:0px 7px 0px 0px;"
+                        :style="{'box-shadow': `${faceColor} 0px 0px 0px 0.5px inset`}"
+                    />
                 </div>
-                <div
-                    :style="{'box-shadow': `${faceColor} 0px 0px 0px 0.5px inset`}"
-                    style="position:absolute;left:0;right:0;top:0;bottom:0;border-radius:6px 6px 6px 6px;"
-                ></div>
             </v-layout>
             <span
                 class="caption font-weight-bold text-truncate"
@@ -117,3 +95,16 @@ function hash(str: string) {
     return h
 }
 </script>
+<style scoped>
+.badge {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 21px;
+    height: 21px;
+    clip-path: url(#app-button-badge);
+    background-color: white;
+    overflow: hidden;
+}
+</style>
+
