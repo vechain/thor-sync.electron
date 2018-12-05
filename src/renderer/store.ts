03bb7888d68373fe286d339ffcc0536f12014a10
qianbin <cola.tin.com@gmail.com>
2018-12-05 14:37:10 +0800

    add shortcuts to store

--- src/renderer/store.ts
+++ src/renderer/store.ts
@@ -1,27 +1,36 @@
 import Vuex from 'vuex'
+import { Entities } from './database'
 
 namespace Store {
     export type Model = {
         chainStatus: Connex.Thor.Status
+        shortcuts: Array<Entities.Preference<'shortcut'>>
     }
 }
 
 class Store extends Vuex.Store<Store.Model> {
     public static readonly UPDATE_CHAIN_STATUS = 'updateChainStatus'
+    public static readonly UPDATE_SHORTCUTS = 'updateShortcuts'
     constructor() {
         super({
             state: {
                 chainStatus: connex.thor.status,
+                shortcuts: []
             },
             getters: {
             },
             mutations: {
                 [Store.UPDATE_CHAIN_STATUS](state) {
                     state.chainStatus = connex.thor.status
+                },
+                [Store.UPDATE_SHORTCUTS](state, payload) {
+                    state.shortcuts = payload
                 }
             }
         })
         this.monitorChain()
+        this.monitorDB()
+
     }
 
     private async monitorChain() {
@@ -32,6 +41,21 @@ class Store extends Vuex.Store<Store.Model> {
             this.commit(Store.UPDATE_CHAIN_STATUS)
         }
     }
+
+    private async monitorDB() {
+        const updateShortcuts = async () => {
+            const shortcuts = await DB.preferences
+                .where({ key: 'shortcut' })
+                .toArray()
+            this.commit(Store.UPDATE_SHORTCUTS, shortcuts)
+        }
+
+        await updateShortcuts()
+
+        DB.subscribe(DB.preferences.name, updateShortcuts)
+    }
+
+
 }
 
 export default Store
