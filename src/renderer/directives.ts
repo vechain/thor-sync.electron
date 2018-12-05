8ba1ab5f18ae9357ceb1460396d86c0ad766d8d8
Asbert Ma <stayinalive@msn.cn>
2018-12-05 14:32:27 +0800

    give directives unbind method

--- src/renderer/directives.ts
+++ src/renderer/directives.ts
@@ -2,23 +2,54 @@ import { Vue } from 'vue-property-decorator'
 import { clipboard } from 'electron'
 
 function write(str: string) {
-    clipboard.writeText(str)
+  clipboard.writeText(str)
 }
-Vue.directive('clipboard', {
-    bind(el: HTMLElement, binding: any) {
-        el.addEventListener('click', () => {
-            write(binding.value)
-        })
+
+declare global {
+  interface HTMLElement {
+    _onclick?: {
+      callback: () => void;
     }
+    _focusout?: {
+      callback: (event: any) => void;
+    }
+  }
+}
+
+Vue.directive('clipboard', {
+  bind(el: HTMLElement, binding: any) {
+    const callback = () => {
+      write(binding.value)
+    }
+
+    el.addEventListener('click', callback)
+    el._onclick = {
+      callback
+    }
+  },
+  unbind(el: HTMLElement) {
+    if (el._onclick) {
+      el.removeEventListener('click', el._onclick!.callback)
+    }
+  }
 })
 
 Vue.directive('nofocusout', {
-    bind(el: HTMLElement, binding: any) {
-        el.tabIndex = -1
-        el.addEventListener('focusout', (event: any) => {
-            if (!el.contains(event.relatedTarget)) {
-                el.focus()
-            }
-        })
+  bind(el: HTMLElement, binding: any) {
+    el.tabIndex = -1
+    const callback = (event: any) => {
+      if (!el.contains(event.relatedTarget)) {
+        el.focus()
+      }
     }
+    el.addEventListener('focusout', callback)
+    el._focusout = {
+      callback
+    }
+  },
+  unbind(el: HTMLElement) {
+    if (el._focusout) {
+      el.removeEventListener('focusout', el._onclick!.callback)
+    }
+  }
 })
