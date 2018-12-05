14f57d9abb820d1e0cdd6e1dcc5fdea4aab0e3fb
Asbert Ma <stayinalive@msn.cn>
2018-12-05 15:34:00 +0800

    component as a package

---
+++ src/renderer/components/index.ts
@@ -0,0 +1,53 @@
+import AccessHistoryPanel from './AccessHistoryPanel.vue'
+import Activity from './Activity.vue'
+import AddressLabel from './AddressLabel.vue'
+import Amount from './Amount.vue'
+import AppButton from './AppButton.vue'
+import CertIndicator from './CertIndicator.vue'
+import ClauseItem from './ClauseItem.vue'
+import ConfirmDialog from './ConfirmDialog.vue'
+import Favicon from './Favicon.vue'
+import IdentBox from './IdentBox.vue'
+import NetworkStatus from './NetworkStatus.vue'
+import NetworkStatusPanel from './NetworkStatusPanel.vue'
+import OverlayedMenu from './OverlayedMenu.vue'
+import Priority from './Priority.vue'
+import QRCode from './QRCode.vue'
+import QRCodeDialog from './QRCodeDialog.vue'
+import Swiper from './Swiper.vue'
+import TabButton from './TabButton.vue'
+import Tip from './Tip.vue'
+import TxRecord from './TxRecord.vue'
+import TxRecordsPanel from './TxRecordsPanel.vue'
+import UrlBox from './UrlBox.vue'
+import WalletCard from './WalletCard.vue'
+import WalletSeeker from './WalletSeeker.vue'
+import WebView from './WebView.vue'
+
+export {
+    AccessHistoryPanel,
+    Activity,
+    AddressLabel,
+    Amount,
+    AppButton,
+    CertIndicator,
+    ClauseItem,
+    ConfirmDialog,
+    Favicon,
+    IdentBox,
+    NetworkStatus,
+    NetworkStatusPanel,
+    OverlayedMenu,
+    Priority,
+    QRCode,
+    QRCodeDialog,
+    Swiper,
+    TabButton,
+    Tip,
+    TxRecord,
+    TxRecordsPanel,
+    UrlBox,
+    WalletCard,
+    WalletSeeker,
+    WebView
+}
