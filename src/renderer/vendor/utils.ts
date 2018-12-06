b80ae5269c81270aa60391fb3f77b7b4f4d3f889
qianbin <cola.tin.com@gmail.com>
2018-12-06 17:14:35 +0800

    use RLP kind to validate tx msg

--- src/renderer/vendor/utils.ts
+++ src/renderer/vendor/utils.ts
@@ -1,4 +1,4 @@
-import { cry } from 'thor-devkit'
+import { cry, RLP } from 'thor-devkit'
 
 function assert(cond: boolean, msg: string) {
     if (!cond) {
@@ -6,13 +6,13 @@ function assert(cond: boolean, msg: string) {
     }
 }
 
-function isHex(str: string) {
-    return /^0x[0-9a-f]*$/i.test(str)
-}
-
+const toKind = new RLP.NullableFixedBlobKind(20)
+const valueKind = new RLP.NumericKind(32)
+const dataKind = new RLP.BlobKind()
 
 export function normalizeMsg(msg: Connex.Vendor.SigningService.TxMessage) {
     assert(Array.isArray(msg), 'expected array')
+
     return msg.map((c, i) => {
         c = { ...c }
         c.to = c.to || null
@@ -20,27 +20,11 @@ export function normalizeMsg(msg: Connex.Vendor.SigningService.TxMessage) {
         c.data = c.data || '0x'
         c.comment = c.comment || ''
 
-        if (c.to) {
-            assert(cry.isAddress(c.to), `#${i}.to expected address or null`)
-        }
+        toKind.data(c.to, `#${i}.to`)
+        valueKind.data(c.value, `#${i}.value`)
+        dataKind.data(c.data, `#${i}.data`)
 
-        if (c.value) {
-            assert(typeof c.value === 'string' || typeof c.value === 'number',
-                `#${i}.value expected string or number`)
-            if (typeof c.value === 'string') {
-                assert(isHex(c.value) && c.value.length > 2,
-                    `#${i}.value expected non-negative integer in hex string`)
-                c.value = c.value.toLowerCase()
-            } else {
-                assert(Number.isSafeInteger(c.value) && c.value >= 0,
-                    `#${i} expected non-negative safe integer`)
-            }
-        }
-
-        assert(isHex(c.data) && c.data.length % 2 === 0,
-            `#${i} expected odd hex string`)
-
-        assert(typeof c.comment === 'string', `#${i} expected string`)
+        assert(typeof c.comment === 'string', `#${i}.comment expected string`)
         return c
     })
 }
