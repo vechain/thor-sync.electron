f9b270d59a28285a2f91a17c06f5f4b7299bddf2
Asbert Ma <stayinalive@msn.cn>
2018-12-10 18:08:34 +0800

    settings

--- src/renderer/launcher/Update.vue
+++ src/renderer/launcher/Update.vue
@@ -2,31 +2,27 @@
     <v-container>
         <v-layout row justify-center>
             <v-flex sm12 lg6>
+                <h3 class="headline">VeChainSync</h3>
                 <v-card>
-                    <v-card-title class="headline">Setting</v-card-title>
-                    <v-card-text
-                        class="grey--text text--darken-1"
-                    >"Sync" releases updates from time to time. Install the update version for feature, security fixes, and improved software stability.</v-card-text>
+                    <v-card-text class="grey--text text--darken-1">Version {{getVersion()}}</v-card-text>
                     <v-list>
                         <v-list-tile>
                             <v-list-tile-content>
                                 <v-list-tile-title class="grey--text text--darken-3">Auto Update</v-list-tile-title>
                             </v-list-tile-content>
-                            <!-- <v-list-tile-content> -->
                             <v-list-tile-action>
                                 <v-switch @change="onChange" v-model="isAuto.value"></v-switch>
                             </v-list-tile-action>
-                            <!-- </v-list-tile-content> -->
                         </v-list-tile>
                         <v-list-tile>
                             <v-list-tile-content>
-                                <v-list-tile-title
-                                    class="grey--text text--darken-3"
-                                >Current Software Version</v-list-tile-title>
+                                <v-list-tile-title class="grey--text text--darken-3">Report an issue</v-list-tile-title>
                             </v-list-tile-content>
-                            <v-list-tile-action-text
-                                class="grey--text text--darken-3"
-                            >Current Software Version</v-list-tile-action-text>
+                            <v-list-tile-action>
+                                <v-btn @click.stop="" icon ripple style="margin-right: 20px">
+                                    <v-icon>launch</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
                         </v-list-tile>
                     </v-list>
                 </v-card>
@@ -35,44 +31,52 @@
     </v-container>
 </template>
 <script lang="ts">
-import { Vue, Component, Watch } from 'vue-property-decorator'
-import { State } from 'vuex-class'
-import { Entities } from '../database'
+    import { Vue, Component, Watch, Emit } from 'vue-property-decorator'
+    import { State } from 'vuex-class'
+    import { Entities } from '../database'
+    import { remote } from 'electron'
 
-@Component
-export default class AutoUpdate extends Vue {
-    name = 'auto_update'
-    isAuto: any = {
-        key: 'auto-update',
-        value: true
-    }
-    @State
-    preferencesRevision!: number
+    @Component
+    export default class AutoUpdate extends Vue {
+        name = 'auto_update'
+        isAuto: any = {
+            key: 'auto-update',
+            value: true
+        }
 
-    async created() {
-        await this.getConfig()
-    }
+        getVersion() {
+            return remote.app.getVersion()
+        }
+        @State
+        preferencesRevision!: number
 
-    onChange() {
-        if (this.isAuto.id) {
-            GDB.preferences.update(this.isAuto.id as any, {
-                value: this.isAuto.value
-            })
-        } else {
-            GDB.preferences.add(this.isAuto)
+        async created() {
+            await this.getConfig()
+        }
+
+        @Emit('')
+        reportIssue(link: string){}
+
+        onChange() {
+            if (this.isAuto.id) {
+                GDB.preferences.update(this.isAuto.id as any, {
+                    value: this.isAuto.value
+                })
+            } else {
+                GDB.preferences.add(this.isAuto)
+            }
+        }
+
+        @Watch('preferencesRevision')
+        async getConfig() {
+            let temp = await GDB.preferences
+                .where('key')
+                .equals('auto-update')
+                .first()
+            if (temp) {
+                this.isAuto = temp
+            }
         }
     }
-
-    @Watch('preferencesRevision')
-    async getConfig() {
-        let temp = await GDB.preferences
-            .where('key')
-            .equals('auto-update')
-            .first()
-        if (temp) {
-            this.isAuto = temp
-        }
-    }
-}
 </script>
 
