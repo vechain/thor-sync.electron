75a052e865c9e12341ea2c88e4320b84878e5b1c
qianbin <cola.tin.com@gmail.com>
2018-12-11 17:38:20 +0800

    update NetworkStatusPanel to display all networks

--- src/renderer/components/NetworkStatusPanel.vue
+++ src/renderer/components/NetworkStatusPanel.vue
@@ -4,24 +4,29 @@
         offset-y
         :close-on-content-click="false"
         max-height="500px"
-        max-width="200px"
-        min-width="180px"
         v-model="opened"
     >
         <slot slot="activator" name="activator"/>
-        <v-card flat>
-            <v-container>
-                <v-layout column>
-                    <v-flex>
-                        <v-icon small>mdi-clock</v-icon>
-                        <span>{{progressText}}</span>
-                    </v-flex>
-                    <v-flex>
-                        <v-icon small>mdi-animation</v-icon>
-                        <span>{{headNumberText}}</span>
-                    </v-flex>
+        <v-card flat width="250">
+            <v-card-text>
+                <v-layout row align-center>
+                    <div class="text-truncate title" style="flex:1 1 auto">{{config.name}}</div>
+                    <v-btn icon small class="ma-0" @click="onSettings">
+                        <v-icon small>mdi-settings</v-icon>
+                    </v-btn>
                 </v-layout>
-            </v-container>
+                <div class="caption text-truncate grey--text">{{config.url}}</div>
+            </v-card-text>
+            <v-card-text class="pt-0">
+                <v-layout align-center>
+                    <v-icon small class="mr-2">mdi-cube</v-icon>
+                    {{headNumberText}}
+                </v-layout>
+                <v-layout align-center>
+                    <v-icon small class="mr-2">mdi-swap-vertical-bold</v-icon>
+                    {{progressText}}
+                </v-layout>
+            </v-card-text>
             <v-list>
                 <template v-for="site in otherConfigs">
                     <v-divider :key="`${site.genesis.id}@${site.url}-divider`"></v-divider>
@@ -45,27 +50,36 @@ import { Vue, Component } from 'vue-property-decorator'
 import { remote } from 'electron'
 import { State } from 'vuex-class'
 import siteConfigs from '../../site-configs'
+import { Entities } from '@/renderer/database';
 
 type Status = Connex.Thor.Status
 
 @Component
 export default class NetworkStatusPanel extends Vue {
-    otherConfigs = siteConfigs
-        .filter(
-            c => {
-                const config = remote.getCurrentWebContents().getWebPreferences().siteConfig!
-                return c.url !== config.url || c.genesis.id !== config.genesis.id
-            })
+    config = remote.getCurrentWebContents().getWebPreferences().siteConfig!
+
+    get configs() {
+        const networks = this.$store.state.networks as Entities.Preference<'network'>[]
+        return [...siteConfigs, ...networks.map(n => n.value)]
+    }
+
+    get otherConfigs() {
+        return this.configs.filter(c => c.url !== this.config.url || c.genesis.id !== this.config.genesis.id)
+    }
 
     @State chainStatus!: Status
     opened = false
 
     get progressText() {
-        return (Math.round(this.chainStatus.progress * 10000) / 100).toString() + '%'
+        if (this.chainStatus.progress === 1) {
+            return 'Synced'
+        }
+
+        return `Syncing (${Math.floor(this.chainStatus.progress * 10000) / 100}%)`
     }
 
     get headNumberText() {
-        return '#' + this.chainStatus.head.number.toLocaleString()
+        return this.chainStatus.head.number.toLocaleString()
     }
 
     activateOrOpenWindow(config: Thor.Site.Config) {
@@ -84,6 +98,10 @@ export default class NetworkStatusPanel extends Vue {
         } else {
             remote.app.EXTENSION.createWindow(config)
         }
+    }
+    onSettings() {
+        BUS.$emit('open-tab', { href: 'sync://settings', mode: 'inplace-builtin' })
+        this.opened = false
     }
 }
 </script>
