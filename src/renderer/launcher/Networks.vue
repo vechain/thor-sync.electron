1c27fd1d7f644e178fa4f4ff19a36d31f672d0b2
Asbert Ma <stayinalive@msn.cn>
2018-12-11 18:20:08 +0800

    settings layout

--- src/renderer/launcher/Networks.vue
+++ src/renderer/launcher/Networks.vue
@@ -1,39 +1,33 @@
 <template>
-    <v-container>
-        <v-layout row justify-center>
-            <v-flex sm12 lg6 row wrap="">
-                <v-subheader>
-                    <span class="subheading">Networks</span>
-                    <v-spacer />
-                    <a @click="dialog = true"
-                        style="float: right; margin-right: 20px; margin-top: 5px;"
-                    >Add</a>
-                </v-subheader>
-                <v-card>
-                    <v-card-title v-if="!rows.length">
-                        <h3 class="subheading">No network items!</h3>
-                    </v-card-title>
-                    <v-list v-if="rows.length" two-line subheader>
-                        <div v-for="(item, index) in rows" :key="index">
-                            <v-list-tile avatar>
-                                <v-list-tile-content>
-                                    <v-list-tile-title>{{item.value.name}}</v-list-tile-title>
-                                    <v-list-tile-sub-title>{{item.value.url}}</v-list-tile-sub-title>
-                                </v-list-tile-content>
-                                <v-list-tile-action>
-                                    <v-btn icon @click="onEdit(item)" ripple dark color="#82B1FF">
-                                        <v-icon small>edit</v-icon>
-                                    </v-btn>
-                                </v-list-tile-action>
-                            </v-list-tile>
-                            <v-divider v-if="index !== (rows.length - 1)"></v-divider>
-                        </div>
-                    </v-list>
-                </v-card>
-                <NewNetworkDialog @cancel="onCancelEdit" :editItem="editItem" v-model="dialog"/>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <v-layout column>
+        <v-subheader>
+            <span class="subheading">Networks</span>
+            <v-spacer/>
+            <a @click="dialog = true" style="float: right; margin-right: 20px; margin-top: 5px;">Add</a>
+        </v-subheader>
+        <v-card>
+            <v-card-title v-if="!rows.length">
+                <h3 class="subheading">No network items!</h3>
+            </v-card-title>
+            <v-list v-if="rows.length" two-line subheader>
+                <div v-for="(item, index) in rows" :key="index">
+                    <v-list-tile avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{item.value.name}}</v-list-tile-title>
+                            <v-list-tile-sub-title>{{item.value.url}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn icon @click="onEdit(item)" ripple dark color="#82B1FF">
+                                <v-icon small>edit</v-icon>
+                            </v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                    <v-divider v-if="index !== (rows.length - 1)"></v-divider>
+                </div>
+            </v-list>
+        </v-card>
+        <NewNetworkDialog @cancel="onCancelEdit" :editItem="editItem" v-model="dialog"/>
+    </v-layout>
 </template>
 <script lang="ts">
     import { Vue, Component, Watch, Mixins } from 'vue-property-decorator'
