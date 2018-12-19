9b127f36aaeeca7bbf952c9e7cecce854e0f12ad
Asbert Ma <stayinalive@msn.cn>
2018-12-19 18:20:22 +0800

    account mixin

---
+++ src/renderer/mixins/Account.ts
@@ -0,0 +1,26 @@
+import { Vue, Component } from 'vue-property-decorator'
+import { cry } from 'thor-devkit'
+@Component
+export default class AccountMixin extends Vue {
+  public error: {
+    isError: boolean;
+    messages: string[];
+  } = {
+    isError: false,
+    messages: []
+  }
+
+  public async checkPwd(password: string, ks?: cry.Keystore) {
+    if (ks) {
+      try {
+        return await cry.Keystore.decrypt(ks, password)
+      } catch (error) {
+        this.error.isError = true
+        this.error.messages = ['Password is invalid']
+        console.error(error)
+
+        return null
+      }
+    }
+  }
+}
