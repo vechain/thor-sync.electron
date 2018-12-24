be0c57b590a6d29b7e58359ff2603f0699addfa0
qianbin <cola.tin.com@gmail.com>
2018-12-24 22:22:46 +0800

    add transition for ExpansionBtn

--- src/renderer/components/ExpansionBtn.vue
+++ src/renderer/components/ExpansionBtn.vue
@@ -1,9 +1,14 @@
 <template>
     <v-btn v-bind="$attrs" v-on="$listeners" @click="onClick" @blur="onBlur">
         <slot/>
-        <span v-show="expanded">
-            <slot name="expansion"/>
-        </span>
+        <div
+            style="overflow: hidden;position:relative;transition:all 0.2s;transition-timing-function:ease"
+            :style="contentStyleObject"
+        >
+            <div ref="contentWrapper" style="position:absolute;left:0;top:0;">
+                <slot name="expansion"/>
+            </div>
+        </div>
     </v-btn>
 </template>
 <script lang="ts">
@@ -13,10 +18,18 @@ import { Vue, Prop, Emit, Component } from 'vue-property-decorator'
 export default class ExpansionBtn extends Vue {
     @Emit('action')
     emitAction() { }
-
     expanded = false
-
     blurTimer: any
+
+    contentSize = { x: 0, y: 0 };
+
+    get contentStyleObject() {
+        return {
+            width: this.expanded ? this.contentSize.x + 'px' : '0px',
+            height: this.contentSize.y + 'px',
+            opacity: this.expanded ? 1 : 0
+        }
+    }
 
     onClick() {
         if (this.blurTimer) {
@@ -34,6 +47,10 @@ export default class ExpansionBtn extends Vue {
     }
     onBlur() {
         this.blurTimer = setTimeout(() => this.expanded = false, 100)
+    }
+    beforeUpdate() {
+        this.contentSize.x = (this.$refs.contentWrapper as HTMLElement).clientWidth
+        this.contentSize.y = (this.$refs.contentWrapper as HTMLElement).clientHeight
     }
 }    
 </script>
