f1f3a93f9fe07f249174f0f2703a7af96a4f2c7f
qianbin <cola.tin.com@gmail.com>
2018-12-27 17:01:57 +0800

    add entities into sync.d.ts

--- src/sync.d.ts
+++ src/sync.d.ts
@@ -38,3 +38,74 @@ declare namespace VendorInterface {
         signer?: string
     }
 }
+
+type Keystore = {
+    address: string;
+    crypto: object;
+    id: string;
+    version: number;
+}
+
+declare namespace entities {
+    type Activity<T extends 'tx' | 'cert'> = {
+        id?: number
+        type: T
+        createdTime: number
+        referer: Referer
+        closed: number
+        data:
+        T extends 'tx' ? Activity.Tx :
+        T extends 'cert' ? Activity.Cert : never
+    }
+
+    namespace Activity {
+        type Tx = {
+            id: string
+            message: Connex.Vendor.SigningService.TxMessage
+            comment: string
+            timestamp: number
+            signer: string
+            estimatedFee: string
+            link: string
+            raw: string
+            receipt: Connex.Thor.Receipt | null
+        }
+
+        type Cert = {
+            message: Connex.Vendor.SigningService.CertMessage
+            signer: string
+            timestamp: number
+            domain: string
+            signature: string
+        }
+    }
+
+    interface Preference {
+        id: number
+        key: string
+        value: any
+    }
+
+    type Shortcut = {
+        id: number
+        title: string
+        href: string
+    }
+
+    type Node = {
+        id: number
+    } & NodeConfig
+
+    interface AccessRecord {
+        id: number
+        baseUrl: string
+    }
+
+    interface Wallet {
+        id?: number
+        address: string
+        name: string
+        keystore: Keystore
+        createdTime: number
+    }
+}
