b16f56a832d4828b4a1547e29b087549c134700b
qianbin <cola.tin.com@gmail.com>
2019-01-12 02:04:11 +0800

    refactor addressLabel amount and QRCode

--- src/renderer/components/AddressLabel.vue
+++ src/renderer/components/AddressLabel.vue
@@ -1,68 +1,46 @@
+<template>
+    <span v-if="!slotContent">{{placeholder}}</span>
+    <span v-else-if="!address">invalid address</span>
+    <div v-else-if="icon" class="white outline" style="overflow:hidden;display:inline-block">
+        <IdentBox :text="slotContent.toLowerCase()" style="height:55%;"/>
+    </div>
+    <span v-else style="font-family: 'Roboto Mono', monospace">{{address}}</span>
+</template>
 <script lang="ts">
-
-import { Vue } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import { Address } from '@/common/formatter'
 import { VNodeData } from 'vue'
 import IdentBox from './IdentBox.vue'
 
-export default Vue.extend({
-    components: {
-        IdentBox
-    },
-    props: {
-        icon: Boolean,
-        abbrev: Boolean,
-        placeholder: {
-            type: String,
-            default: ''
-        }
-    },
-    render(h) {
-        const data: VNodeData = {
-            attrs: this.$attrs,
-            on: this.$listeners,
-        }
+@Component
+export default class AddressLabel extends Vue {
+    @Prop(Boolean) icon!: boolean
+    @Prop(Boolean) abbrev !: Boolean
+    @Prop(String) placeholder !: string
 
-        if (!this.$slots.default) {
-            data.domProps = { innerText: this.placeholder }
-            return h('span', data)
-        }
-        const addr = (this.$slots.default[0].text || '').trim()
-        if (!addr) {
-            data.domProps = { innerText: this.placeholder }
-            return h('span', data)
-        }
-        if (!Address.isValid(addr)) {
-            data.domProps = { innerText: 'invalid address' }
-            return h('span', data)
-        }
+    slotContent = ''
 
-        if (this.icon) {
-            data.style = {
-                display: 'inline-block',
-                overflow: 'hidden'
-            }
-            data.class = "white outline"
-            return h('div', data, [h('IdentBox', {
-                props: {
-                    text: addr.toLowerCase()
-                },
-                style: {
-                    height: '55%'
-                }
-            })])
-        } else {
-            const checksumed = Address.toChecksum(addr)!
-            if (this.abbrev) {
-                data.domProps = { innerText: Address.abbrev(checksumed) }
-            } else {
-                data.domProps = { innerText: checksumed }
-            }
-            data.style = {
-                'font-family': '"Roboto Mono", monospace'
-            }
-            return h('span', data)
+    get address() {
+        if (!Address.isValid(this.slotContent)) {
+            return undefined
         }
-    },
-})
+        const checksumed = Address.toChecksum(this.slotContent)!
+        if (this.abbrev) {
+            return Address.abbrev(checksumed)!
+        }
+        return checksumed
+    }
+
+    created() {
+        this.extractSlot()
+    }
+    beforeUpdate() {
+
+        this.extractSlot()
+    }
+
+    extractSlot() {
+        this.slotContent = this.$slots.default[0] ? (this.$slots.default[0].text || '').trim() : ''
+    }
+}
 </script>
