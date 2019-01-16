21309c28304a68c00303563f0b7ee96ce41f46b1
qianbin <cola.tin.com@gmail.com>
2019-01-17 01:40:28 +0800

    improve launcher view

--- src/renderer/launcher/Launcher.vue
+++ src/renderer/launcher/Launcher.vue
@@ -1,81 +1,27 @@
 <template>
     <div>
-        <div id="content"/>
+        <component ref="builtin" :is="_builtinClass" :href="href" :nav="nav" v-on="$listeners"/>
     </div>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop, Watch, Model, Emit } from 'vue-property-decorator'
-import Router from './Router.vue'
+import BuiltinBase, { builtinClass } from './Builtin.vue'
 
 
 @Component
 export default class Launcher extends Vue {
-    readonly router = Router.create(this)
-
     @Prop(String) href!: string
-    @Emit("update:href")
-    updateHref(val: string) { }
-
-    @Watch('href')
-    hrefChanged(val: string) {
-        const path = hrefToPath(val)
-        if (this.router.$route.path !== path) {
-            this.router.$router.push(path)
-        }
-    }
-
-    @Emit('update:status')
-    updateStatus(status: WebView.Status) { }
-
-    @Watch('router.$route.path')
-    routed() {
-        let path = this.router.$route.path
-        if (path === '/') {
-            this.updateHref('')
-        } else {
-            this.updateHref('sync:/' + path)
-        }
-        let favicon
-        if (path.includes('wallets')) {
-            favicon = 'mdi-cards'
-        } else if (path.includes('settings')) {
-            favicon = 'mdi-settings'
-        } else if (path.includes('transfer')) {
-            favicon = 'mdi-transfer'
-        } else {
-            favicon = 'mdi-home'
-        }
-
-        const history = this.router.$router.history
-        this.updateStatus({
-            title: (this.router.$route.meta || {}).title || '',
-            favicon,
-            progress: 1,
-            canGoBack: history.index !== 0,
-            canGoForward: history.index < history.stack.length - 1,
-            cert: null
-        })
-    }
     @Prop(Object) nav!: WebView.Nav
-    @Watch('nav.goBack')
-    goBack() { this.router.$router.back() }
-    @Watch('nav.goForward')
-    goForward() { this.router.$router.forward() }
+
+    builtin!: BuiltinBase
+    _builtinClass!: typeof BuiltinBase
+
+    beforeCreate() {
+        this._builtinClass = builtinClass()
+    }
 
     mounted() {
-        this.router.$mount('#content')
-        this.router.$router.push(hrefToPath(this.href || ''))
-        this.routed()
+        this.builtin = this.$refs.builtin as any
     }
-    destroyed() {
-        this.router.$destroy()
-    }
-}
-
-function hrefToPath(href: string) {
-    if (href.startsWith('sync:/')) {
-        return href.slice('sync:/'.length)
-    }
-    return href
 }
 </script>
