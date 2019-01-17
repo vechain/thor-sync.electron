371b4f7b5536d28a350a1f51979c6636cb8a3d55
Asbert Ma <stayinalive@msn.cn>
2019-01-17 11:14:55 +0800

    adjust delete dialog

--- src/renderer/components/DeleteWalletDialog.vue
+++ src/renderer/components/DeleteWalletDialog.vue
@@ -1,83 +1,101 @@
 <template>
-    <DialogEx v-model="show" @action:cancel="show=false" max-width="400px">
-        <v-form @submit.prevent="onNext">
-            <v-card>
-                <v-card-title class="subheading">Delete Wallet</v-card-title>
-                <div class="font-weight-bold pl-4 pr-4">Unless you have backed up your wallet beforehand, your wallet will be permanently deleted. we will not be able to help you restore it</div>
-                <v-card-text>
-                    <v-text-field
-                        :error="error.isError"
-                        :error-messages="error.messages"
-                        type="password"
-                        v-focus
-                        label="Password"
-                        v-model="password"
-                        :loading="checking"
-                    >
-                        <v-progress-linear v-if="checking" slot="progress" indeterminate height="2"></v-progress-linear>
-                    </v-text-field>
-                </v-card-text>
-                <v-divider/>
-                <v-card-actions>
-                    <v-btn small flat @click.stop="close">Abort</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn small flat type="submit" color="error">Delete</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-form>
+    <DialogEx persistent v-model="show" @action:ok="onNext" @action:cancel="show=false" max-width="400px">
+        <v-card>
+            <v-card-title class="subheading">Delete Wallet</v-card-title>
+            <v-card-text>
+                <div>
+                    This action
+                    <strong>CANNOT </strong>be undone. Unless you have backed up your wallet beforehand, your wallet will be
+                    <strong>PERMANENTLY </strong>deleted.
+                </div>
+                <br>
+                <v-text-field
+                    :error="error.isError"
+                    :error-messages="error.messages"
+                    type="text"
+                    v-focus
+                    placeholder="Please type in the name of the wallet to confirm"
+                    label="Wallet Name"
+                    v-model="name"
+                    :loading="checking"
+                ></v-text-field>
+            </v-card-text>
+            <v-divider/>
+            <v-card-actions>
+                <v-btn small flat @click.stop="close">Abort</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn small flat type="button" @click="onNext" class="error">Delete this wallet</v-btn>
+            </v-card-actions>
+        </v-card>
     </DialogEx>
 </template>
 <script lang="ts">
-    import { Vue, Component, Watch, Prop, Mixins } from 'vue-property-decorator'
-    import { cry } from 'thor-devkit'
-    import AccountMixin from '@/renderer/mixins/Account'
-    import DialogHelper from '@/renderer/mixins/dialog-helper'
-    import { setTimeout } from 'timers'
-    @Component
-    export default class DeleteWalletDialog extends Mixins(
-        AccountMixin,
-        class extends DialogHelper<entities.Wallet, void> {}
-    ) {
-        password = ''
-        show = false
-        checking = false
-        error: {
-            isError: boolean
-            messages: string[]
-        } = {
-            isError: false,
-            messages: []
-        }
+import { Vue, Component, Watch, Prop, Mixins } from 'vue-property-decorator'
+import { cry } from 'thor-devkit'
+import AccountMixin from '@/renderer/mixins/Account'
+import DialogHelper from '@/renderer/mixins/dialog-helper'
+import { setTimeout } from 'timers'
 
-        mounted() {
-            this.show = true
-        }
+type Arg = {
+    name: string
+    id?: number
+}
+@Component
+export default class DeleteWalletDialog extends Mixins(
+    class extends DialogHelper<Arg, void> {}
+) {
+    name = ''
+    show = false
+    checking = false
+    error: {
+        isError: boolean
+        messages: string[]
+    } = {
+        isError: false,
+        messages: []
+    }
 
-        @Watch('show')
-        showChanged(val: boolean) {
-            if (!val) {
-                this.$resolve(undefined)
-            }
-        }
+    mounted() {
+        this.show = true
+    }
 
-        async onNext() {
-            this.checking = true
-            if (await this.checkPwd(this.password, this.arg.keystore)) {
-                await this.deleteWallet()
-            }
-            this.checking = false
-        }
-
-        close() {
-            this.show = false
-        }
-
-        async deleteWallet() {
-            if (this.arg) {
-                await BDB.wallets.delete(this.arg.id || 0)
-                this.close()
-            }
+    @Watch('show')
+    showChanged(val: boolean) {
+        if (!val) {
+            this.$resolve(undefined)
         }
     }
+
+    checkName() {
+        if (this.name === this.arg.name) {
+            this.error.isError = false
+            this.error.messages = []
+            return true
+        } else {
+            this.error.isError = true
+            this.error.messages = ['Invalid wallet name']
+            return false
+        }
+    }
+
+    async onNext() {
+        this.checking = true
+        if (this.checkName()) {
+            await this.deleteWallet()
+        }
+        this.checking = false
+    }
+
+    close() {
+        this.show = false
+    }
+
+    async deleteWallet() {
+        if (this.arg) {
+            await BDB.wallets.delete(this.arg.id || 0)
+            this.close()
+        }
+    }
+}
 </script>
 
