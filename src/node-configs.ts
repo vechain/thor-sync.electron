e629d272c5a5b3dfd132658dae04e4f015ed6d9c
qianbin <cola.tin.com@gmail.com>
2019-01-25 10:52:35 +0800

    add isTrunk to preset node configs to fit connex standard

--- src/node-configs.ts
+++ src/node-configs.ts
@@ -15,7 +15,8 @@ export const presets: NodeConfig[] = [{
         stateRoot: '0x09bfdf9e24dd5cd5b63f3c1b5d58b97ff02ca0490214a021ed7d99b93867839c',
         receiptsRoot: '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
         signer: '0x0000000000000000000000000000000000000000',
-        transactions: []
+        transactions: [],
+        isTrunk: true
     }
 }, {
     name: 'vechain.org',
@@ -34,7 +35,8 @@ export const presets: NodeConfig[] = [{
         stateRoot: '0x4ec3af0acbad1ae467ad569337d2fe8576fe303928d35b8cdd91de47e9ac84bb',
         receiptsRoot: '0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0',
         signer: '0x0000000000000000000000000000000000000000',
-        transactions: []
+        transactions: [],
+        isTrunk: true
     }
 }]
 
