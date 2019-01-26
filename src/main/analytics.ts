8daec76f1f1e516bf33e1e83f463410fc4c1fe3b
qianbin <cola.tin.com@gmail.com>
2019-01-27 01:35:18 +0800

    extract analytics codes

---
+++ src/main/analytics.ts
@@ -0,0 +1,46 @@
+import env from '@/env'
+import * as Path from 'path'
+import { app } from 'electron'
+import { readFile, writeFile } from 'fs'
+import UUID from 'uuid'
+import * as log from 'electron-log'
+
+export class Analytics {
+    private readonly ga: any
+    private clientId?: string
+    constructor() {
+        // tslint:disable-next-line:variable-name
+        const GA = require('electron-google-analytics')
+        this.ga = new GA.default('UA-132391998-1', { debug: env.devMode })
+    }
+
+    public initClientId() {
+        const path = Path.resolve(app.getPath('userData'), 'client-id')
+        return new Promise<string>(resolve => {
+            if (this.clientId) {
+                return resolve(this.clientId)
+            }
+            readFile(path, 'utf8', (err, data) => {
+                if (this.clientId) {
+                    return resolve(this.clientId)
+                }
+                if (err) {
+                    this.clientId = UUID.v4()
+                    writeFile(path, this.clientId, 'utf8', _err => {
+                        log.error('failed to write client id', _err)
+                    })
+                    return resolve(this.clientId)
+                }
+                this.clientId = data
+                return resolve(data)
+            })
+        })
+    }
+
+    public startup() {
+        this.initClientId().then(id => {
+            this.ga.event(`app-${app.getVersion()}`, 'startup', { clientID: id })
+        })
+    }
+}
+
