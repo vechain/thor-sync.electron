a7788c8fd3fbc539c3b5c9ef7a6108ba58681403
qianbin <cola.tin.com@gmail.com>
2019-01-27 02:06:42 +0800

    update dev wallets

---
+++ src/dev-wallets.ts
@@ -0,0 +1,80 @@
+const array: entities.Wallet[] = [
+    {
+        address: '0xf2e7617c45c42967fde0514b5aa6bba56e3e11dd',
+        name: 'Foo',
+        keystore: {
+            address: 'f2e7617c45c42967fde0514b5aa6bba56e3e11dd',
+            crypto: {
+                cipher: 'aes-128-ctr',
+                ciphertext: '6c1f2a2c24cf538a158b6259cf9b0622232660abb22e981a92d7f812432b0aa3',
+                cipherparams: {
+                    iv: '449d081478fb2d1d430099e5920c1db4'
+                },
+                mac: 'd5ceb32bfb3ba0d154a0326a399ef194301db838b53a419cd1a637b941f20e42',
+                kdf: 'scrypt',
+                kdfparams: {
+                    dklen: 32,
+                    n: 262144,
+                    r: 8,
+                    p: 1,
+                    salt: 'd75e43884c4ffbaa259e76a3c112d17118c47064a333997d3e90c2b251aa3dfe'
+                }
+            },
+            id: '169940ad-ce0d-4997-9b23-b950565569f1',
+            version: 3
+        },
+        createdTime: Date.now()
+    }, {
+        address: '0x4b202889ad775c0fb975949fb2a4aeb7547db374',
+        name: 'Baz',
+        keystore: {
+            address: '4b202889ad775c0fb975949fb2a4aeb7547db374',
+            crypto: {
+                cipher: 'aes-128-ctr',
+                ciphertext: '67d0558e0ad1da9efffa840e4d5d6ea68e3c3430033e7e1ff8966ac0ea9bb8ad',
+                cipherparams: {
+                    iv: '2363c71136c2d1a42669fc434f77f509'
+                },
+                mac: 'd88808084fa2ebf276b68cdec796e41a4742ba3533921a44b9f705d61253a8ed',
+                kdf: 'scrypt',
+                kdfparams: {
+                    dklen: 32,
+                    n: 262144,
+                    r: 8,
+                    p: 1,
+                    salt: 'db5fd60eeec0d8b540715758ea5fb3a841daeea7d365dbf09bc2cbed9c329315'
+                }
+            },
+            id: 'e6916ccc-6b5f-4ea5-9d5b-1e68b2a415c5',
+            version: 3
+        },
+        createdTime: Date.now()
+    }, {
+        address: '0xfa6e63168115a9202dcd834f6c20eabf48f18ba7',
+        name: 'Bar',
+        keystore: {
+            address: 'fa6e63168115a9202dcd834f6c20eabf48f18ba7',
+            crypto: {
+                cipher: 'aes-128-ctr',
+                ciphertext: 'fbdd9cffbf78f605e63191f4785867ab79fe9c6eb5fef2b646e8f4f661596ab7',
+                cipherparams: {
+                    iv: 'bc9ca32e266422e310f418c67b01b213'
+                },
+                mac: 'e302737146832392dc173e770caab55ef309727af595c8e2816d778208168941',
+                kdf: 'scrypt',
+                kdfparams: {
+                    dklen: 32,
+                    n: 262144,
+                    r: 8,
+                    p: 1,
+                    salt: '9a21a24241fedb99400b01751bc141b87949b0ba4a4d5fdbbb1d94c0d44681d6'
+                }
+            },
+            id: 'f6c72ded-c725-4c63-b267-1fb7bc1b7de3',
+            version: 3
+        },
+        createdTime: Date.now()
+    }
+]
+
+export default array
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -10,6 +10,7 @@ import VueRouter from 'vue-router'
 import Nova from './Nova.vue'
 import Store from './store'
 import DialogHelper from './mixins/dialog-helper'
+import DevWallets from '../dev-wallets'
 
 Vue.use(Vuetify, {
     iconfont: 'mdi' // 'md' || 'mdi' || 'fa' || 'fa4'
@@ -51,33 +52,7 @@ if (isFirstTimeRun) {
 
     if (ENV.devMode) {
         BDB.on('ready', () => {
-            BDB.wallets.put({
-                address: '0xf6e78a5584c06e2dec5c675d357f050a5402a730',
-                name: 'Foo',
-                keystore: JSON.parse(
-                    // tslint:disable-next-line:max-line-length
-                    '{"address":"f6e78a5584c06e2dec5c675d357f050a5402a730","crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"ddbdf9ccf12446fb15a5252d27ff46ae"},"ciphertext":"637cc65de2515c4d9e52b60ffd944de79b5b2adedc322a8d713daf32c592ae53","kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"1449a3cbaf300a030a4f6e7e60dcce9367a3fee5ee9079d7a363c5972bdb14b6"},"mac":"cba0573d099c976213900d4751e8b96eb622c58dc0c2e9b37860db95aeb35081"},"id":"e4e54c43-3935-4541-8e73-66d3C289842F","version":3}'
-                ),
-                createdTime: Date.now()
-            })
-            BDB.wallets.put({
-                address: '0x881abd9fe12319e9353f45ed47472cffb34e375e',
-                name: 'Bar',
-                keystore: JSON.parse(
-                    // tslint:disable-next-line:max-line-length
-                    '{"version":3,"id":"147a3e15-638a-473b-836d-5c14903acfa3","address":"881abd9fe12319e9353f45ed47472cffb34e375e","crypto":{"kdf":"scrypt","kdfparams":{"dklen":32,"salt":"6239b7ad3b52fb800f83d24e9ba0393df277a79d40e1c71f6e510e231b9aece9","n":262144,"r":8,"p":1},"cipher":"aes-128-ctr","ciphertext":"3bc78973893808f9d05004e95552dcbcc5c3af45cde3983dc7eec533b84858d3","cipherparams":{"iv":"67f1857ec6e19c0907b579535f724c84"},"mac":"7f7722bca47ce99cede34215952d37829bf7a4147d14be263c17ce8b85a58ac6"}}'
-                ),
-                createdTime: Date.now()
-            })
-            BDB.wallets.put({
-                address: '0x40d9b870df290c582f71d48bb208c6fa275ac6be',
-                name: 'Baz',
-                keystore: JSON.parse(
-                    // tslint:disable-next-line:max-line-length
-                    '{"version":3,"id":"3efc9991-b02c-4615-bb66-87ddecbeac95","address":"40d9b870df290c582f71d48bb208c6fa275ac6be","crypto":{"kdf":"scrypt","kdfparams":{"dklen":32,"salt":"1b6ace1df4c3696cb47fb743186333a3e3860353f287f64f518e2fb024e65c3c","n":262144,"r":8,"p":1},"cipher":"aes-128-ctr","ciphertext":"f27d7546182e8be49f400150f92352babe165299e0ce08a81d4def441591d24e","cipherparams":{"iv":"4e7ab13269770caf71fc68a230ddc963"},"mac":"876edfd59def69b73baabfaa5d01f96cfa7ba38e60dc5c98fc637f815b3a1760"}}'
-                ),
-                createdTime: Date.now()
-            })
+            BDB.wallets.bulkAdd(DevWallets)
         }, true)
     }
 }
