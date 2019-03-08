49ab4556e1b81b9c41fa97559c2e36ccce9b8ff0
qianbin <cola.tin.com@gmail.com>
2019-03-08 16:34:17 +0800

    improve tx detail panel display

--- src/renderer/components/TxSigningDialog.vue
+++ src/renderer/components/TxSigningDialog.vue
@@ -16,7 +16,7 @@
                     <div class="py-2 px-3">
                         <div class="subheading text-truncate">Transaction</div>
                         <div class="text-truncate">
-                            <i>{{txComment}}</i>
+                            <i :title="txComment">{{txComment}}</i>
                         </div>
                     </div>
                     <v-expansion-panel expand popout class="pa-1" style="overflow:auto;">
