a480e5dc21df9da3935e50f02cf33da4d3e8a644
Asbert Ma <stayinalive@msn.cn>
2019-07-08 16:17:28 +0800

    mnemonic insensitive

--- src/renderer/components/MnemonicWords.vue
+++ src/renderer/components/MnemonicWords.vue
@@ -1,7 +1,7 @@
 <template>
     <v-layout py-2 row wrap style="background: rgba(128,128,0,0.05);" text-xs-center>
         <v-flex xs2 py-2 v-for="word in words" :key="word">
-            <span class="text-uppercase body-2">{{word}}</span>
+            <span class="body-2">{{word}}</span>
         </v-flex>
     </v-layout>
 </template>
