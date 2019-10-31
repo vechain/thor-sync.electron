dafc159ddf0a19dec853f3619fff3fa3b80fe1d8
Asbert Ma <stayinalive@msn.cn>
2019-10-31 15:09:50 +0800

    delete ledger dialog

---
+++ src/renderer/components/DeleteDeviceDialog.vue
@@ -0,0 +1,111 @@
+<template>
+    <DialogEx
+        persistent
+        v-model="show"
+        @action:ok="onOk"
+        @action:cancel="show=false"
+        max-width="450px"
+    >
+        <v-card>
+            <v-card-title class="subheading">Remove Ledger</v-card-title>
+            <v-card-text>
+                <p>
+                    When you remove this Ledger , you
+                    <strong>CANNOT</strong> select this ledger
+                    (<strong>{{arg.name}}</strong>) as a singer.You can import this device if you needed.
+                </p>
+                <v-text-field
+                    ref="nameInput"
+                    type="text"
+                    v-focus
+                    validate-on-blur
+                    placeholder="Please input the device name to confirm"
+                    label="Device Name"
+                    v-model="name"
+                    :rules="[checkName]"
+                    :loading="checking"
+                ></v-text-field>
+            </v-card-text>
+            <v-divider />
+            <v-card-actions>
+                <v-btn small flat @click.stop="close">Abort</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn
+                    small
+                    ref="submit"
+                    flat
+                    type="button"
+                    @click="onNext"
+                    class="error"
+                    :disabled="checking"
+                >Delete this wallet</v-btn>
+            </v-card-actions>
+        </v-card>
+    </DialogEx>
+</template>
+<script lang="ts">
+import { Vue, Component, Watch, Prop, Mixins } from 'vue-property-decorator'
+import { cry } from 'thor-devkit'
+import DialogHelper from '@/renderer/mixins/dialog-helper'
+import { setTimeout } from 'timers'
+
+type Arg = {
+    name: string
+    id?: number
+}
+@Component
+export default class DeleteDeviceDialog extends Mixins(
+    class extends DialogHelper<Arg, void> { }
+) {
+    name = ''
+    show = false
+    checking = false
+
+    mounted() {
+        this.show = true
+    }
+
+    @Watch('show')
+    showChanged(val: boolean) {
+        if (!val) {
+            this.$resolve(undefined)
+        }
+    }
+
+    checkName() {
+        if (this.name === this.arg.name) {
+            return true
+        } else {
+            return 'Invalid device name'
+        }
+    }
+
+    async onNext() {
+        this.checking = true
+        const filed = this.$refs.nameInput as any
+
+        if (filed.validate()) {
+            await this.deleteWallet()
+            this.close()
+        }
+        this.checking = false
+    }
+
+    close() {
+        this.show = false
+    }
+
+    async deleteWallet() {
+        if (this.arg) {
+            await LDDB.devices.delete(this.arg.id || 0)
+        }
+    }
+
+    onOk() {
+        const btn = (this.$refs.submit as Vue).$el
+        btn.focus()
+        btn.click()
+    }
+}
+</script>
+
